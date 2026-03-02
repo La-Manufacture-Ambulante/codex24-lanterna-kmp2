f1147b2c890f8543a623b89b36d306b2e6dd4628
WS24 Agent <ws24-agent@local>
2026-03-02 19:23:37 +0000

    convert OutputString to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/examples/OutputString.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/examples/OutputString.kt
@@ -1,31 +1,31 @@
-package com.googlecode.lanterna.examples;
+package com.googlecode.lanterna.examples
 
-import java.io.IOException;
+import java.io.IOException
+import com.googlecode.lanterna.graphics.TextGraphics
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
 
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
+class OutputString {
+    companion object {
+        @JvmStatic
+        @Throws(IOException::class)
+        fun main(args: Array<String?>?) {
+            val terminal: Terminal = DefaultTerminalFactory().createTerminal()
+            val screen: Screen = TerminalScreen(terminal)
 
+            val s: String = "Hello World!"
+            val tGraphics: TextGraphics = screen.newTextGraphics()
 
-public class OutputString {
+            screen.startScreen()
+            screen.clear()
 
-	public static void main(String[] args) throws IOException {
-		Terminal terminal = new DefaultTerminalFactory().createTerminal();
-		Screen screen = new TerminalScreen(terminal);
+            tGraphics.putString(10, 10, s)
+            screen.refresh()
 
-		String s = "Hello World!";
-		TextGraphics tGraphics = screen.newTextGraphics();
-
-		screen.startScreen();
-		screen.clear();
-
-		tGraphics.putString(10, 10, s);
-		screen.refresh();
-
-		screen.readInput();
-		screen.stopScreen();
-	}
-
+            screen.readInput()
+            screen.stopScreen()
+        }
+    }
 }
