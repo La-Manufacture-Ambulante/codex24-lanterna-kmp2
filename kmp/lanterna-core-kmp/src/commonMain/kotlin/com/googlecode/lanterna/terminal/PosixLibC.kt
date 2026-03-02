6b14bc629b090a47e1ec0d84150729191a813fe7
WS24 Agent <ws24-agent@local>
2026-03-02 19:14:56 +0000

    convert PosixLibC to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/PosixLibC.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/PosixLibC.kt
@@ -1,125 +1,113 @@
-/*
- * This file is part of lanterna (https://github.com/mabe02/lanterna).
- *
- * lanterna is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2010-2024 Martin Berglund
- */
-package com.googlecode.lanterna.terminal;
+package com.googlecode.lanterna.terminal
 
-import com.sun.jna.Callback;
-import com.sun.jna.Library;
-import com.sun.jna.Structure;
+import com.sun.jna.Callback
+import com.sun.jna.Library
+import com.sun.jna.Structure
 
-import java.util.Arrays;
-import java.util.List;
+interface PosixLibC : Library {
+    fun tcgetattr(fd: Int, termios_p: termios?): Int
+    fun tcsetattr(fd: Int, optional_actions: Int, termios_p: termios?): Int
+    fun ioctl(fd: Int, request: Int, winsize: winsize?): Int
+    fun signal(sig: Int, fn: sig_t?): sig_t?
 
-/**
- * Interface to Posix libc
- */
-public interface PosixLibC extends Library {
-    int tcgetattr(int fd, termios termios_p);
-    int tcsetattr(int fd, int optional_actions, termios termios_p);
-    int ioctl(int fd, int request, winsize winsize);
-    sig_t signal(int sig, sig_t fn);
+    companion object {
+        const val STDIN_FILENO: Int = 0
+        const val STDOUT_FILENO: Int = 1
+        const val TCSANOW: Int = 0
+        const val NCCS: Int = 32
 
-    // Constants
-    int STDIN_FILENO = 0;
-    int STDOUT_FILENO = 1;
-    int TCSANOW = 0;
-    int NCCS = 32;
+        // Constants for c_lflag (converted from Java octal literals: 01, 02, 010)
+        const val ISIG: Int = 1
+        const val ICANON: Int = 2
+        const val ECHO: Int = 8
 
-    // Constants for c_lflag (beware of octal numbers below!!)
-    @SuppressWarnings("OctalInteger")
-    int ISIG = 01;
-    @SuppressWarnings("OctalInteger")
-    int ICANON = 02;
-    @SuppressWarnings("OctalInteger")
-    int ECHO = 010;
+        // Signals
+        const val SIGWINCH: Int = 28
 
-    // Signals
-    int SIGWINCH = 28;
-
-    // Constants for ioctl
-    int TIOCGWINSZ = 0x5413;
-
-    interface sig_t extends Callback {
-        void invoke(int signal);
+        // Constants for ioctl
+        const val TIOCGWINSZ: Int = 0x5413
     }
 
-    class termios extends Structure {
-        public int c_iflag;           // input mode flags
-        public int c_oflag;           // output mode flags
-        public int c_cflag;           // control mode flags
-        public int c_lflag;           // local mode flags
-        public byte c_line;           // line discipline
-        public byte c_cc[];           // control characters
-        public int c_ispeed;          // input speed
-        public int c_ospeed;          // output speed
+    interface sig_t : Callback {
+        fun invoke(signal: Int)
+    }
 
-        public termios() {
-            c_cc = new byte[NCCS];
+    class termios : Structure() {
+        @JvmField
+        var c_iflag: Int = 0 // input mode flags
+
+        @JvmField
+        var c_oflag: Int = 0 // output mode flags
+
+        @JvmField
+        var c_cflag: Int = 0 // control mode flags
+
+        @JvmField
+        var c_lflag: Int = 0 // local mode flags
+
+        @JvmField
+        var c_line: Byte = 0 // line discipline
+
+        @JvmField
+        var c_cc: ByteArray? = ByteArray(PosixLibC.NCCS) // control characters
+
+        @JvmField
+        var c_ispeed: Int = 0 // input speed
+
+        @JvmField
+        var c_ospeed: Int = 0 // output speed
+
+        override fun getFieldOrder(): List<String> {
+            return listOf(
+                "c_iflag",
+                "c_oflag",
+                "c_cflag",
+                "c_lflag",
+                "c_line",
+                "c_cc",
+                "c_ispeed",
+                "c_ospeed"
+            )
         }
 
-        protected List getFieldOrder() {
-            return Arrays.asList(
-                    "c_iflag",
-                    "c_oflag",
-                    "c_cflag",
-                    "c_lflag",
-                    "c_line",
-                    "c_cc",
-                    "c_ispeed",
-                    "c_ospeed"
-            );
-        }
-
-        @Override
-        public String toString() {
+        override fun toString(): String {
             return "termios{" +
-                    "c_iflag=" + c_iflag +
-                    ", c_oflag=" + c_oflag +
-                    ", c_cflag=" + c_cflag +
-                    ", c_lflag=" + c_lflag +
-                    ", c_line=" + c_line +
-                    ", c_cc=" + Arrays.toString(c_cc) +
-                    ", c_ispeed=" + c_ispeed +
-                    ", c_ospeed=" + c_ospeed +
-                    '}';
+                "c_iflag=" + c_iflag +
+                ", c_oflag=" + c_oflag +
+                ", c_cflag=" + c_cflag +
+                ", c_lflag=" + c_lflag +
+                ", c_line=" + c_line +
+                ", c_cc=" + (c_cc?.contentToString() ?: "null") +
+                ", c_ispeed=" + c_ispeed +
+                ", c_ospeed=" + c_ospeed +
+                '}'
         }
     }
 
-    class winsize extends Structure
-    {
-        public short ws_row;
-        public short ws_col;
-        public short ws_xpixel;
-        public short ws_ypixel;
+    class winsize : Structure() {
+        @JvmField
+        var ws_row: Short = 0
 
-        @Override
-        protected List getFieldOrder() {
-            return Arrays.asList("ws_row", "ws_col", "ws_xpixel", "ws_ypixel");
+        @JvmField
+        var ws_col: Short = 0
+
+        @JvmField
+        var ws_xpixel: Short = 0
+
+        @JvmField
+        var ws_ypixel: Short = 0
+
+        override fun getFieldOrder(): List<String> {
+            return listOf("ws_row", "ws_col", "ws_xpixel", "ws_ypixel")
         }
 
-        @Override
-        public String toString() {
+        override fun toString(): String {
             return "winsize{" +
-                    "ws_row=" + ws_row +
-                    ", ws_col=" + ws_col +
-                    ", ws_xpixel=" + ws_xpixel +
-                    ", ws_ypixel=" + ws_ypixel +
-                    '}';
+                "ws_row=" + ws_row +
+                ", ws_col=" + ws_col +
+                ", ws_xpixel=" + ws_xpixel +
+                ", ws_ypixel=" + ws_ypixel +
+                '}'
         }
     }
 }
