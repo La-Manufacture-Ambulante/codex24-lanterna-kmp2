1dba6c52f2e255d1def288541d0d3e22ed3b63b9
WS24 Agent <ws24-agent@local>
2026-03-02 19:20:08 +0000

    convert WindowsTerminal to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/WindowsTerminal.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/WindowsTerminal.kt
@@ -1,137 +1,145 @@
-package com.googlecode.lanterna.terminal;
+package com.googlecode.lanterna.terminal
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.terminal.ansi.UnixLikeTerminal;
-import com.sun.jna.Native;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.win32.W32APIOptions;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
+import com.googlecode.lanterna.TerminalPosition
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.terminal.ansi.UnixLikeTerminal
+import com.sun.jna.Native
+import com.sun.jna.ptr.IntByReference
+import com.sun.jna.win32.W32APIOptions
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
 
 /**
  * Terminal implementation for the regular Windows cmd.exe terminal emulator, using native invocations through jna to
  * interact with it.
  */
-public class WindowsTerminal extends UnixLikeTerminal {
+open class WindowsTerminal @Throws(IOException::class) constructor(
+    terminalInput: InputStream?,
+    terminalOutput: OutputStream?,
+    terminalCharset: Charset?,
+    terminalCtrlCBehaviour: CtrlCBehaviour?
+) : UnixLikeTerminal(
+    terminalInput,
+    terminalOutput,
+    terminalCharset,
+    terminalCtrlCBehaviour
+) {
 
-    private static final Wincon WINDOWS_CONSOLE = (Wincon) Native.loadLibrary("kernel32", Wincon.class, W32APIOptions.UNICODE_OPTIONS);
-    private static final WinDef.HANDLE CONSOLE_INPUT_HANDLE = WINDOWS_CONSOLE.GetStdHandle(Wincon.STD_INPUT_HANDLE);
-    private static final WinDef.HANDLE CONSOLE_OUTPUT_HANDLE = WINDOWS_CONSOLE.GetStdHandle(Wincon.STD_OUTPUT_HANDLE);
+    private var savedTerminalInputMode: Int? = null
+    private var savedTerminalOutputMode: Int? = null
 
-    private Integer savedTerminalInputMode;
-    private Integer savedTerminalOutputMode;
+    @Throws(IOException::class)
+    constructor() : this(
+        System.`in`,
+        System.out,
+        Charset.defaultCharset(),
+        CtrlCBehaviour.CTRL_C_KILLS_APPLICATION
+    )
 
-    public WindowsTerminal() throws IOException {
-        this(System.in, System.out, Charset.defaultCharset(), CtrlCBehaviour.CTRL_C_KILLS_APPLICATION);
+    @Throws(IOException::class)
+    override fun acquire() {
+        super.acquire()
+        var terminalOutputMode = getConsoleOutputMode()
+        terminalOutputMode = terminalOutputMode or Wincon.ENABLE_VIRTUAL_TERMINAL_PROCESSING
+        terminalOutputMode = terminalOutputMode or Wincon.DISABLE_NEWLINE_AUTO_RETURN
+        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_OUTPUT_HANDLE, terminalOutputMode)
     }
 
-    public WindowsTerminal(
-            InputStream terminalInput,
-            OutputStream terminalOutput,
-            Charset terminalCharset,
-            CtrlCBehaviour terminalCtrlCBehaviour) throws IOException {
-
-        super(terminalInput,
-                terminalOutput,
-                terminalCharset,
-                terminalCtrlCBehaviour);
+    @Synchronized
+    @Throws(IOException::class)
+    override fun saveTerminalSettings() {
+        this.savedTerminalInputMode = getConsoleInputMode()
+        this.savedTerminalOutputMode = getConsoleOutputMode()
     }
 
-    @Override
-    protected void acquire() throws IOException {
-        super.acquire();
-        int terminalOutputMode = getConsoleOutputMode();
-        terminalOutputMode |= Wincon.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-        terminalOutputMode |= Wincon.DISABLE_NEWLINE_AUTO_RETURN;
-        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_OUTPUT_HANDLE, terminalOutputMode);
-    }
-
-    @Override
-    public synchronized void saveTerminalSettings() throws IOException {
-        this.savedTerminalInputMode = getConsoleInputMode();
-        this.savedTerminalOutputMode = getConsoleOutputMode();
-    }
-
-    @Override
-    public synchronized void restoreTerminalSettings() throws IOException {
-        if(savedTerminalInputMode != null) {
-            WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, savedTerminalInputMode);
-            WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_OUTPUT_HANDLE, savedTerminalOutputMode);
+    @Synchronized
+    @Throws(IOException::class)
+    override fun restoreTerminalSettings() {
+        if (savedTerminalInputMode != null) {
+            WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, savedTerminalInputMode!!)
+            WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_OUTPUT_HANDLE, savedTerminalOutputMode!!)
         }
     }
 
-    @Override
-    public synchronized void keyEchoEnabled(boolean enabled) throws IOException {
-        int mode = getConsoleInputMode();
-        if(enabled) {
-            mode |= Wincon.ENABLE_ECHO_INPUT;
+    @Synchronized
+    @Throws(IOException::class)
+    override fun keyEchoEnabled(enabled: Boolean) {
+        var mode = getConsoleInputMode()
+        if (enabled) {
+            mode = mode or Wincon.ENABLE_ECHO_INPUT
+        } else {
+            mode = mode and Wincon.ENABLE_ECHO_INPUT.inv()
         }
-        else {
-            mode &= ~Wincon.ENABLE_ECHO_INPUT;
-        }
-        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode);
+        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode)
     }
 
-    @Override
-    public synchronized void canonicalMode(boolean enabled) throws IOException {
-        int mode = getConsoleInputMode();
-        if(enabled) {
-            mode |= Wincon.ENABLE_LINE_INPUT;
+    @Synchronized
+    @Throws(IOException::class)
+    override fun canonicalMode(enabled: Boolean) {
+        var mode = getConsoleInputMode()
+        if (enabled) {
+            mode = mode or Wincon.ENABLE_LINE_INPUT
+        } else {
+            mode = mode and Wincon.ENABLE_LINE_INPUT.inv()
         }
-        else {
-            mode &= ~Wincon.ENABLE_LINE_INPUT;
-        }
-        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode);
+        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode)
     }
 
-    @Override
-    public synchronized void keyStrokeSignalsEnabled(boolean enabled) throws IOException {
-        int mode = getConsoleInputMode();
-        if(enabled) {
-            mode |= Wincon.ENABLE_PROCESSED_INPUT;
+    @Synchronized
+    @Throws(IOException::class)
+    override fun keyStrokeSignalsEnabled(enabled: Boolean) {
+        var mode = getConsoleInputMode()
+        if (enabled) {
+            mode = mode or Wincon.ENABLE_PROCESSED_INPUT
+        } else {
+            mode = mode and Wincon.ENABLE_PROCESSED_INPUT.inv()
         }
-        else {
-            mode &= ~Wincon.ENABLE_PROCESSED_INPUT;
-        }
-        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode);
+        WINDOWS_CONSOLE.SetConsoleMode(CONSOLE_INPUT_HANDLE, mode)
     }
 
-
-    @Override
-    protected TerminalSize findTerminalSize() throws IOException {
-        WinDef.CONSOLE_SCREEN_BUFFER_INFO screenBufferInfo = new WinDef.CONSOLE_SCREEN_BUFFER_INFO();
-        WINDOWS_CONSOLE.GetConsoleScreenBufferInfo(CONSOLE_OUTPUT_HANDLE, screenBufferInfo);
-        int columns = screenBufferInfo.srWindow.Right - screenBufferInfo.srWindow.Left + 1;
-        int rows = screenBufferInfo.srWindow.Bottom - screenBufferInfo.srWindow.Top + 1;
-        return new TerminalSize(columns, rows);
+    @Throws(IOException::class)
+    override fun findTerminalSize(): TerminalSize {
+        val screenBufferInfo = WinDef.CONSOLE_SCREEN_BUFFER_INFO()
+        WINDOWS_CONSOLE.GetConsoleScreenBufferInfo(CONSOLE_OUTPUT_HANDLE, screenBufferInfo)
+        val columns = screenBufferInfo.srWindow.Right - screenBufferInfo.srWindow.Left + 1
+        val rows = screenBufferInfo.srWindow.Bottom - screenBufferInfo.srWindow.Top + 1
+        return TerminalSize(columns, rows)
     }
 
-    @Override
-    public void registerTerminalResizeListener(Runnable runnable) throws IOException {
+    @Throws(IOException::class)
+    override fun registerTerminalResizeListener(runnable: Runnable?) {
         // Not implemented yet
     }
 
-    public synchronized TerminalPosition getCursorPosition() {
-        WinDef.CONSOLE_SCREEN_BUFFER_INFO screenBufferInfo = new WinDef.CONSOLE_SCREEN_BUFFER_INFO();
-        WINDOWS_CONSOLE.GetConsoleScreenBufferInfo(CONSOLE_OUTPUT_HANDLE, screenBufferInfo);
-        int column = screenBufferInfo.dwCursorPosition.X - screenBufferInfo.srWindow.Left;
-        int row = screenBufferInfo.dwCursorPosition.Y - screenBufferInfo.srWindow.Top;
-        return new TerminalPosition(column, row);
+    @Synchronized
+    open fun getCursorPosition(): TerminalPosition {
+        val screenBufferInfo = WinDef.CONSOLE_SCREEN_BUFFER_INFO()
+        WINDOWS_CONSOLE.GetConsoleScreenBufferInfo(CONSOLE_OUTPUT_HANDLE, screenBufferInfo)
+        val column = screenBufferInfo.dwCursorPosition.X - screenBufferInfo.srWindow.Left
+        val row = screenBufferInfo.dwCursorPosition.Y - screenBufferInfo.srWindow.Top
+        return TerminalPosition(column, row)
     }
 
-    private int getConsoleInputMode() {
-        IntByReference lpMode = new IntByReference();
-        WINDOWS_CONSOLE.GetConsoleMode(CONSOLE_INPUT_HANDLE, lpMode);
-        return lpMode.getValue();
+    private fun getConsoleInputMode(): Int {
+        val lpMode = IntByReference()
+        WINDOWS_CONSOLE.GetConsoleMode(CONSOLE_INPUT_HANDLE, lpMode)
+        return lpMode.value
     }
 
-    private int getConsoleOutputMode() {
-        IntByReference lpMode = new IntByReference();
-        WINDOWS_CONSOLE.GetConsoleMode(CONSOLE_OUTPUT_HANDLE, lpMode);
-        return lpMode.getValue();
+    private fun getConsoleOutputMode(): Int {
+        val lpMode = IntByReference()
+        WINDOWS_CONSOLE.GetConsoleMode(CONSOLE_OUTPUT_HANDLE, lpMode)
+        return lpMode.value
+    }
+
+    companion object {
+        private val WINDOWS_CONSOLE: Wincon =
+            Native.loadLibrary("kernel32", Wincon::class.java, W32APIOptions.UNICODE_OPTIONS) as Wincon
+        private val CONSOLE_INPUT_HANDLE: WinDef.HANDLE =
+            WINDOWS_CONSOLE.GetStdHandle(Wincon.STD_INPUT_HANDLE)
+        private val CONSOLE_OUTPUT_HANDLE: WinDef.HANDLE =
+            WINDOWS_CONSOLE.GetStdHandle(Wincon.STD_OUTPUT_HANDLE)
     }
 }
