e72aabf1adda151921d0151834fcc28f3de448d8
WS24 Agent <ws24-agent@local>
2026-03-02 19:26:13 +0000

    convert TerminalRectangle to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/TerminalRectangle.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/TerminalRectangle.kt
@@ -1,122 +1,82 @@
-/*
- * This file is part of lanterna (https://github.com/mabe02/lanterna).
- *
- * lanterna is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2010-2024 Martin Berglund
- */
-package com.googlecode.lanterna;
-
-import java.util.Objects;
+package com.googlecode.lanterna
 
 /**
  * This class is immutable and cannot change its internal state after creation.
  *
  * @author ginkoblongata
  */
-public class TerminalRectangle {
-    
+class TerminalRectangle(x: Int, y: Int, width: Int, height: Int) {
+
     // one of the benefits of immutable: ease of usage
-    public final TerminalPosition position;
-    public final TerminalSize size;
-    public final int x;
-    public final int y;
-    public final int width;
-    public final int height;
-    
-    public final int xAndWidth;
-    public final int yAndHeight;
-    
-    /**
-     * Creates a new terminal rect representation at the supplied x y position with the supplied width and height.
-     *
-     * Both width and height must be at least zero (non negative) as checked in TerminalSize.
-     *
-     * @param width number of columns
-     * @param height number of rows
-     */
-    public TerminalRectangle(int x, int y, int width, int height) {
-        position = new TerminalPosition(x, y);
-        size = new TerminalSize(width, height);
-        
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.xAndWidth = x + width;
-        this.yAndHeight = y + height;
-    }
-    
+    val position: TerminalPosition = TerminalPosition(x, y)
+    val size: TerminalSize = TerminalSize(width, height)
+    val x: Int = x
+    val y: Int = y
+    val width: Int = width
+    val height: Int = height
+
+    val xAndWidth: Int = x + width
+    val yAndHeight: Int = y + height
+
     /**
      * @return Returns the width of this rect, in number of columns
      */
-    public int getColumns() {
-        return width;
+    fun getColumns(): Int {
+        return width
     }
-    
+
     /**
      * @return Returns the height of this rect representation, in number of rows
      */
-    public int getRows() {
-        return height;
+    fun getRows(): Int {
+        return height
     }
-    
+
     /**
      * Creates a new rect based on this rect, but with a different width
      * @param columns Width of the new rect, in columns
      * @return New rect based on this one, but with a new width
      */
-    public TerminalRectangle withColumns(int columns) {
-        return new TerminalRectangle(x, y, columns, height);
+    fun withColumns(columns: Int): TerminalRectangle {
+        return TerminalRectangle(x, y, columns, height)
     }
-    
+
     /**
      * Creates a new rect based on this rect, but with a different height
      * @param rows Height of the new rect, in rows
      * @return New rect based on this one, but with a new height
      */
-    public TerminalRectangle withRows(int rows) {
-        return new TerminalRectangle(x, y, width, rows);
+    fun withRows(rows: Int): TerminalRectangle {
+        return TerminalRectangle(x, y, width, rows)
     }
 
-    public boolean whenContains(TerminalPosition p, Runnable op) {
-        return whenContains(p.getColumn(), p.getRow(), op);
+    fun whenContains(p: TerminalPosition?, op: Runnable?): Boolean {
+        return whenContains(p!!.getColumn(), p.getRow(), op)
     }
-    public boolean whenContains(int x, int y, Runnable op) {
+
+    fun whenContains(x: Int, y: Int, op: Runnable?): Boolean {
         if (this.x <= x && x < this.xAndWidth && this.y <= y && y < this.yAndHeight) {
-            op.run();
-            return true;
+            op!!.run()
+            return true
         }
-        return false;
+        return false
     }
 
-
-    @Override
-    public String toString() {
-        return "{x: " + x + ", y: " + y + ", width: " + width + ", height: " + height + "}";
+    override fun toString(): String {
+        return "{x: $x, y: $y, width: $width, height: $height}"
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj != null
-            && obj.getClass() == getClass()
-            && Objects.equals(position, ((TerminalRectangle)obj).position)
-            && Objects.equals(size, ((TerminalRectangle)obj).size);
+    override fun equals(obj: Any?): Boolean {
+        return obj != null &&
+            obj.javaClass == javaClass &&
+            position == (obj as TerminalRectangle).position &&
+            size == obj.size
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(position, size);
+    override fun hashCode(): Int {
+        var result = 1
+        result = 31 * result + position.hashCode()
+        result = 31 * result + size.hashCode()
+        return result
     }
 }
