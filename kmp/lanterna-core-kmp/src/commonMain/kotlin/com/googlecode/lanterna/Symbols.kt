ea1d395b1ee73f94716b26456d5cce7bcc6f9068
WS24 Agent <ws24-agent@local>
2026-03-02 19:24:32 +0000

    convert Symbols to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/Symbols.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/Symbols.kt
@@ -1,326 +1,90 @@
-/*
- * This file is part of lanterna (https://github.com/mabe02/lanterna).
- *
- * lanterna is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2010-2020 Martin Berglund
- */
+package com.googlecode.lanterna
 
-package com.googlecode.lanterna;
+class Symbols private constructor() {
+    companion object {
+        @JvmField val FACE_WHITE: Char = '\u263A'
+        @JvmField val FACE_BLACK: Char = '\u263B'
+        @JvmField val HEART: Char = '\u2665'
+        @JvmField val CLUB: Char = '\u2663'
+        @JvmField val DIAMOND: Char = '\u2666'
+        @JvmField val SPADES: Char = '\u2660'
+        @JvmField val BULLET: Char = '\u2022'
+        @JvmField val INVERSE_BULLET: Char = '\u25D8'
+        @JvmField val WHITE_CIRCLE: Char = '\u25CB'
+        @JvmField val INVERSE_WHITE_CIRCLE: Char = '\u25D9'
 
-/**
- * Some text graphics, taken from http://en.wikipedia.org/wiki/Codepage_437 but converted to its UTF-8 counterpart.
- * This class it mostly here to help out with building text GUIs when you don't have a handy Unicode chart available.
- * Previously this class was known as ACS, which was taken from ncurses (meaning "Alternative Character Set").
- * @author martin
- */
-public class Symbols {
-    private Symbols() {}
+        @JvmField val SOLID_SQUARE: Char = '\u25A0'
+        @JvmField val SOLID_SQUARE_SMALL: Char = '\u25AA'
+        @JvmField val OUTLINED_SQUARE: Char = '\u25A1'
+        @JvmField val OUTLINED_SQUARE_SMALL: Char = '\u25AB'
 
-    /**
-     * ☺
-     */
-    public static final char FACE_WHITE = 0x263A;
-    /**
-     * ☻
-     */
-    public static final char FACE_BLACK = 0x263B;
-    /**
-     * ♥
-     */
-    public static final char HEART = 0x2665;
-    /**
-     * ♣
-     */
-    public static final char CLUB = 0x2663;
-    /**
-     * ♦
-     */
-    public static final char DIAMOND = 0x2666;
-    /**
-     * ♠
-     */
-    public static final char SPADES = 0x2660;
-    /**
-     * •
-     */
-    public static final char BULLET = 0x2022;
-    /**
-     * ◘
-     */
-    public static final char INVERSE_BULLET = 0x25d8;
-    /**
-     * ○
-     */
-    public static final char WHITE_CIRCLE = 0x25cb;
-    /**
-     * ◙
-     */
-    public static final char INVERSE_WHITE_CIRCLE = 0x25d9;
+        @JvmField val FEMALE: Char = '\u2640'
+        @JvmField val MALE: Char = '\u2642'
 
-    /**
-     * ■
-     */
-    public static final char SOLID_SQUARE = 0x25A0;
-    /**
-     * ▪
-     */
-    public static final char SOLID_SQUARE_SMALL = 0x25AA;
-    /**
-     * □
-     */
-    public static final char OUTLINED_SQUARE = 0x25A1;
-    /**
-     * ▫
-     */
-    public static final char OUTLINED_SQUARE_SMALL = 0x25AB;
+        @JvmField val ARROW_UP: Char = '\u2191'
+        @JvmField val ARROW_DOWN: Char = '\u2193'
+        @JvmField val ARROW_RIGHT: Char = '\u2192'
+        @JvmField val ARROW_LEFT: Char = '\u2190'
 
-    /**
-     * ♀
-     */
-    public static final char FEMALE = 0x2640;
-    /**
-     * ♂
-     */
-    public static final char MALE = 0x2642;
+        @JvmField val BLOCK_SOLID: Char = '\u2588'
+        @JvmField val BLOCK_DENSE: Char = '\u2593'
+        @JvmField val BLOCK_MIDDLE: Char = '\u2592'
+        @JvmField val BLOCK_SPARSE: Char = '\u2591'
 
-    /**
-     * ↑
-     */
-    public static final char ARROW_UP = 0x2191;
-    /**
-     * ↓
-     */
-    public static final char ARROW_DOWN = 0x2193;
-    /**
-     * →
-     */
-    public static final char ARROW_RIGHT = 0x2192;
-    /**
-     * ←
-     */
-    public static final char ARROW_LEFT = 0x2190;
+        @JvmField val TRIANGLE_RIGHT_POINTING_BLACK: Char = '\u25BA'
+        @JvmField val TRIANGLE_LEFT_POINTING_BLACK: Char = '\u25C4'
+        @JvmField val TRIANGLE_UP_POINTING_BLACK: Char = '\u25B2'
+        @JvmField val TRIANGLE_DOWN_POINTING_BLACK: Char = '\u25BC'
 
-    /**
-     * █
-     */
-    public static final char BLOCK_SOLID = 0x2588;
-    /**
-     * ▓
-     */
-    public static final char BLOCK_DENSE = 0x2593;
-    /**
-     * ▒
-     */
-    public static final char BLOCK_MIDDLE = 0x2592;
-    /**
-     * ░
-     */
-    public static final char BLOCK_SPARSE = 0x2591;
+        @JvmField val TRIANGLE_RIGHT_POINTING_MEDIUM_BLACK: Char = '\u23F4'
+        @JvmField val TRIANGLE_LEFT_POINTING_MEDIUM_BLACK: Char = '\u23F5'
+        @JvmField val TRIANGLE_UP_POINTING_MEDIUM_BLACK: Char = '\u23F6'
+        @JvmField val TRIANGLE_DOWN_POINTING_MEDIUM_BLACK: Char = '\u23F7'
 
-    /**
-     * ►
-     */
-    public static final char TRIANGLE_RIGHT_POINTING_BLACK = 0x25BA;
-    /**
-     * ◄
-     */
-    public static final char TRIANGLE_LEFT_POINTING_BLACK = 0x25C4;
-    /**
-     * ▲
-     */
-    public static final char TRIANGLE_UP_POINTING_BLACK = 0x25B2;
-    /**
-     * ▼
-     */
-    public static final char TRIANGLE_DOWN_POINTING_BLACK = 0x25BC;
+        @JvmField val SINGLE_LINE_HORIZONTAL: Char = '\u2500'
+        @JvmField val BOLD_SINGLE_LINE_HORIZONTAL: Char = '\u2501'
+        @JvmField val BOLD_TO_NORMAL_SINGLE_LINE_HORIZONTAL: Char = '\u257E'
+        @JvmField val BOLD_FROM_NORMAL_SINGLE_LINE_HORIZONTAL: Char = '\u257C'
+        @JvmField val DOUBLE_LINE_HORIZONTAL: Char = '\u2550'
+        @JvmField val SINGLE_LINE_VERTICAL: Char = '\u2502'
+        @JvmField val BOLD_SINGLE_LINE_VERTICAL: Char = '\u2503'
+        @JvmField val BOLD_TO_NORMAL_SINGLE_LINE_VERTICAL: Char = '\u257F'
+        @JvmField val BOLD_FROM_NORMAL_SINGLE_LINE_VERTICAL: Char = '\u257D'
+        @JvmField val DOUBLE_LINE_VERTICAL: Char = '\u2551'
 
-    /**
-     * ⏴
-     */
-    public static final char TRIANGLE_RIGHT_POINTING_MEDIUM_BLACK = 0x23F4;
-    /**
-     * ⏵
-     */
-    public static final char TRIANGLE_LEFT_POINTING_MEDIUM_BLACK = 0x23F5;
-    /**
-     * ⏶
-     */
-    public static final char TRIANGLE_UP_POINTING_MEDIUM_BLACK = 0x23F6;
-    /**
-     * ⏷
-     */
-    public static final char TRIANGLE_DOWN_POINTING_MEDIUM_BLACK = 0x23F7;
+        @JvmField val SINGLE_LINE_TOP_LEFT_CORNER: Char = '\u250C'
+        @JvmField val DOUBLE_LINE_TOP_LEFT_CORNER: Char = '\u2554'
+        @JvmField val SINGLE_LINE_TOP_RIGHT_CORNER: Char = '\u2510'
+        @JvmField val DOUBLE_LINE_TOP_RIGHT_CORNER: Char = '\u2557'
 
+        @JvmField val SINGLE_LINE_BOTTOM_LEFT_CORNER: Char = '\u2514'
+        @JvmField val DOUBLE_LINE_BOTTOM_LEFT_CORNER: Char = '\u255A'
+        @JvmField val SINGLE_LINE_BOTTOM_RIGHT_CORNER: Char = '\u2518'
+        @JvmField val DOUBLE_LINE_BOTTOM_RIGHT_CORNER: Char = '\u255D'
 
-    /**
-     * ─
-     */
-    public static final char SINGLE_LINE_HORIZONTAL = 0x2500;
-    /**
-     * ━
-     */
-    public static final char BOLD_SINGLE_LINE_HORIZONTAL = 0x2501;
-    /**
-     * ╾
-     */
-    public static final char BOLD_TO_NORMAL_SINGLE_LINE_HORIZONTAL = 0x257E;
-    /**
-     * ╼
-     */
-    public static final char BOLD_FROM_NORMAL_SINGLE_LINE_HORIZONTAL = 0x257C;
-    /**
-     * ═
-     */
-    public static final char DOUBLE_LINE_HORIZONTAL = 0x2550;
-    /**
-     * │
-     */
-    public static final char SINGLE_LINE_VERTICAL = 0x2502;
-    /**
-     * ┃
-     */
-    public static final char BOLD_SINGLE_LINE_VERTICAL = 0x2503;
-    /**
-     * ╿
-     */
-    public static final char BOLD_TO_NORMAL_SINGLE_LINE_VERTICAL = 0x257F;
-    /**
-     * ╽
-     */
-    public static final char BOLD_FROM_NORMAL_SINGLE_LINE_VERTICAL = 0x257D;
-    /**
-     * ║
-     */
-    public static final char DOUBLE_LINE_VERTICAL = 0x2551;
+        @JvmField val SINGLE_LINE_CROSS: Char = '\u253C'
+        @JvmField val DOUBLE_LINE_CROSS: Char = '\u256C'
+        @JvmField val DOUBLE_LINE_HORIZONTAL_SINGLE_LINE_CROSS: Char = '\u256A'
+        @JvmField val DOUBLE_LINE_VERTICAL_SINGLE_LINE_CROSS: Char = '\u256B'
 
-    /**
-     * ┌
-     */
-    public static final char SINGLE_LINE_TOP_LEFT_CORNER = 0x250C;
-    /**
-     * ╔
-     */
-    public static final char DOUBLE_LINE_TOP_LEFT_CORNER = 0x2554;
-    /**
-     * ┐
-     */
-    public static final char SINGLE_LINE_TOP_RIGHT_CORNER = 0x2510;
-    /**
-     * ╗
-     */
-    public static final char DOUBLE_LINE_TOP_RIGHT_CORNER = 0x2557;
+        @JvmField val SINGLE_LINE_T_UP: Char = '\u2534'
+        @JvmField val SINGLE_LINE_T_DOWN: Char = '\u252C'
+        @JvmField val SINGLE_LINE_T_RIGHT: Char = '\u251C'
+        @JvmField val SINGLE_LINE_T_LEFT: Char = '\u2524'
 
-    /**
-     * └
-     */
-    public static final char SINGLE_LINE_BOTTOM_LEFT_CORNER = 0x2514;
-    /**
-     * ╚
-     */
-    public static final char DOUBLE_LINE_BOTTOM_LEFT_CORNER = 0x255A;
-    /**
-     * ┘
-     */
-    public static final char SINGLE_LINE_BOTTOM_RIGHT_CORNER = 0x2518;
-    /**
-     * ╝
-     */
-    public static final char DOUBLE_LINE_BOTTOM_RIGHT_CORNER = 0x255D;
+        @JvmField val SINGLE_LINE_T_DOUBLE_UP: Char = '\u2568'
+        @JvmField val SINGLE_LINE_T_DOUBLE_DOWN: Char = '\u2565'
+        @JvmField val SINGLE_LINE_T_DOUBLE_RIGHT: Char = '\u255E'
+        @JvmField val SINGLE_LINE_T_DOUBLE_LEFT: Char = '\u2561'
 
-    /**
-     * ┼
-     */
-    public static final char SINGLE_LINE_CROSS = 0x253C;
-    /**
-     * ╬
-     */
-    public static final char DOUBLE_LINE_CROSS = 0x256C;
-    /**
-     * ╪
-     */
-    public static final char DOUBLE_LINE_HORIZONTAL_SINGLE_LINE_CROSS = 0x256A;
-    /**
-     * ╫
-     */
-    public static final char DOUBLE_LINE_VERTICAL_SINGLE_LINE_CROSS = 0x256B;
+        @JvmField val DOUBLE_LINE_T_UP: Char = '\u2569'
+        @JvmField val DOUBLE_LINE_T_DOWN: Char = '\u2566'
+        @JvmField val DOUBLE_LINE_T_RIGHT: Char = '\u2560'
+        @JvmField val DOUBLE_LINE_T_LEFT: Char = '\u2563'
 
-    /**
-     * ┴
-     */
-    public static final char SINGLE_LINE_T_UP = 0x2534;
-    /**
-     * ┬
-     */
-    public static final char SINGLE_LINE_T_DOWN = 0x252C;
-    /**
-     * ├
-     */
-    public static final char SINGLE_LINE_T_RIGHT = 0x251c;
-    /**
-     * ┤
-     */
-    public static final char SINGLE_LINE_T_LEFT = 0x2524;
-
-    /**
-     * ╨
-     */
-    public static final char SINGLE_LINE_T_DOUBLE_UP = 0x2568;
-    /**
-     * ╥
-     */
-    public static final char SINGLE_LINE_T_DOUBLE_DOWN = 0x2565;
-    /**
-     * ╞
-     */
-    public static final char SINGLE_LINE_T_DOUBLE_RIGHT = 0x255E;
-    /**
-     * ╡
-     */
-    public static final char SINGLE_LINE_T_DOUBLE_LEFT = 0x2561;
-
-    /**
-     * ╩
-     */
-    public static final char DOUBLE_LINE_T_UP = 0x2569;
-    /**
-     * ╦
-     */
-    public static final char DOUBLE_LINE_T_DOWN = 0x2566;
-    /**
-     * ╠
-     */
-    public static final char DOUBLE_LINE_T_RIGHT = 0x2560;
-    /**
-     * ╣
-     */
-    public static final char DOUBLE_LINE_T_LEFT = 0x2563;
-
-    /**
-     * ╧
-     */
-    public static final char DOUBLE_LINE_T_SINGLE_UP = 0x2567;
-    /**
-     * ╤
-     */
-    public static final char DOUBLE_LINE_T_SINGLE_DOWN = 0x2564;
-    /**
-     * ╟
-     */
-    public static final char DOUBLE_LINE_T_SINGLE_RIGHT = 0x255F;
-    /**
-     * ╢
-     */
-    public static final char DOUBLE_LINE_T_SINGLE_LEFT = 0x2562;
+        @JvmField val DOUBLE_LINE_T_SINGLE_UP: Char = '\u2567'
+        @JvmField val DOUBLE_LINE_T_SINGLE_DOWN: Char = '\u2564'
+        @JvmField val DOUBLE_LINE_T_SINGLE_RIGHT: Char = '\u255F'
+        @JvmField val DOUBLE_LINE_T_SINGLE_LEFT: Char = '\u2562'
+    }
 }
