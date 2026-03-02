6c4b2bc8f3b21bc4101ac2e53a14d6c739cf9b39
WS24 Agent <ws24-agent@local>
2026-03-02 19:23:06 +0000

    convert DrawRectangle to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/examples/DrawRectangle.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/examples/DrawRectangle.kt
@@ -1,40 +1,40 @@
-package com.googlecode.lanterna.examples;
+package com.googlecode.lanterna.examples
 
-import java.io.IOException;
-
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
+import java.io.IOException
+import com.googlecode.lanterna.TerminalPosition
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.graphics.TextGraphics
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
 
 /**
  * Creates a TerminalScreen and a TextGraphics from it and writes a rectangle
  * from the character '*'
- * 
- * @author Peter Borkuti
  *
+ * @author Peter Borkuti
  */
-public class DrawRectangle {
+class DrawRectangle {
+    companion object {
+        @JvmStatic
+        @Throws(IOException::class)
+        fun main(args: Array<String?>?) {
+            val terminal: Terminal = DefaultTerminalFactory().createTerminal()
+            val screen: Screen = TerminalScreen(terminal)
 
-	public static void main(String[] args) throws IOException {
-		Terminal terminal = new DefaultTerminalFactory().createTerminal();
-		Screen screen = new TerminalScreen(terminal);
+            val tGraphics: TextGraphics = screen.newTextGraphics()
 
-		TextGraphics tGraphics = screen.newTextGraphics();
+            screen.startScreen()
+            screen.clear()
 
-		screen.startScreen();
-		screen.clear();
+            tGraphics.drawRectangle(
+                TerminalPosition(3, 3), TerminalSize(10, 10), '*'
+            )
+            screen.refresh()
 
-		tGraphics.drawRectangle(
-			new TerminalPosition(3,3), new TerminalSize(10,10), '*');
-		screen.refresh();
-
-		screen.readInput();
-		screen.stopScreen();
-	}
-
+            screen.readInput()
+            screen.stopScreen()
+        }
+    }
 }
