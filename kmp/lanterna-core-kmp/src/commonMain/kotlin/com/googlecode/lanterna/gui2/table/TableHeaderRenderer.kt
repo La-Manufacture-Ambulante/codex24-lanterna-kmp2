ea69aaabdd6b5b0b08c8ffcfb32c8fa650876694
WS24 Agent <ws24-agent@local>
2026-03-03 12:21:26 +0000

    convert TableHeaderRenderer to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/table/TableHeaderRenderer.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/table/TableHeaderRenderer.kt
@@ -16,17 +16,17 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2.table;
+package com.googlecode.lanterna.gui2.table
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.gui2.TextGUIGraphics;
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.gui2.TextGUIGraphics
 
 /**
  * This interface can be implemented if you want to customize how table headers are drawn.
- * @param <V> Type of data stored in each table cell
+ * @param V Type of data stored in each table cell
  * @author Martin
  */
-public interface TableHeaderRenderer<V> {
+interface TableHeaderRenderer<V> {
     /**
      * Called by the table when it wants to know how big a particular table header should be
      * @param table Table containing the header
@@ -34,16 +34,16 @@ public interface TableHeaderRenderer<V> {
      * @param columnIndex Column index of the header
      * @return Size this renderer would like the header to have
      */
-    TerminalSize getPreferredSize(Table<V> table, String label, int columnIndex);
+    fun getPreferredSize(table: Table<V>?, label: String?, columnIndex: Int): TerminalSize?
 
     /**
      * Called by the table when it's time to draw a header, you can see how much size is available by checking the size
-     * of the {@code textGUIGraphics}. The top-left position of the graphics object is the top-left position of this
+     * of the `textGUIGraphics`. The top-left position of the graphics object is the top-left position of this
      * header.
      * @param table Table containing the header
      * @param label Label for this header
      * @param index Column index of the header
      * @param textGUIGraphics Graphics object to header with
      */
-    void drawHeader(Table<V> table, String label, int index, TextGUIGraphics textGUIGraphics);
+    fun drawHeader(table: Table<V>?, label: String?, index: Int, textGUIGraphics: TextGUIGraphics?)
 }
