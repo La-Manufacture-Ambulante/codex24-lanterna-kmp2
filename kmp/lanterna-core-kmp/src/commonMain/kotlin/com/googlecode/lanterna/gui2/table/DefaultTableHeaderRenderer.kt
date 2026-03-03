0ad534a535afe6c441168a66d1f703dfd8fc9460
WS24 Agent <ws24-agent@local>
2026-03-03 12:21:25 +0000

    convert DefaultTableHeaderRenderer to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/table/DefaultTableHeaderRenderer.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/table/DefaultTableHeaderRenderer.kt
@@ -16,30 +16,28 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2.table;
+package com.googlecode.lanterna.gui2.table
 
-import com.googlecode.lanterna.TerminalTextUtils;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.ThemeDefinition;
-import com.googlecode.lanterna.gui2.TextGUIGraphics;
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TerminalTextUtils
+import com.googlecode.lanterna.graphics.ThemeDefinition
+import com.googlecode.lanterna.gui2.TextGUIGraphics
 
 /**
- * Default implementation of {@code TableHeaderRenderer}
+ * Default implementation of `TableHeaderRenderer`
  * @author Martin
  */
-public class DefaultTableHeaderRenderer<V> implements TableHeaderRenderer<V> {
-    @Override
-    public TerminalSize getPreferredSize(Table<V> table, String label, int columnIndex) {
-        if(label == null) {
-            return TerminalSize.ZERO;
+class DefaultTableHeaderRenderer<V> : TableHeaderRenderer<V> {
+    override fun getPreferredSize(table: Table<V>?, label: String?, columnIndex: Int): TerminalSize {
+        if (label == null) {
+            return TerminalSize.ZERO
         }
-        return new TerminalSize(TerminalTextUtils.getColumnWidth(label), 1);
+        return TerminalSize(TerminalTextUtils.getColumnWidth(label), 1)
     }
 
-    @Override
-    public void drawHeader(Table<V> table, String label, int index, TextGUIGraphics textGUIGraphics) {
-        ThemeDefinition themeDefinition = table.getThemeDefinition();
-        textGUIGraphics.applyThemeStyle(themeDefinition.getCustom("HEADER", themeDefinition.getNormal()));
-        textGUIGraphics.putString(0, 0, label);
+    override fun drawHeader(table: Table<V>, label: String?, index: Int, textGUIGraphics: TextGUIGraphics) {
+        val themeDefinition: ThemeDefinition = table.getThemeDefinition()
+        textGUIGraphics.applyThemeStyle(themeDefinition.getCustom("HEADER", themeDefinition.getNormal()))
+        textGUIGraphics.putString(0, 0, label)
     }
 }
