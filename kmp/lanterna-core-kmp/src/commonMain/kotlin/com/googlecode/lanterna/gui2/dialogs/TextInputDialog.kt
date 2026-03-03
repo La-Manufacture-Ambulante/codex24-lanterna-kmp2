aed8d4a0210e0e574ba50ea5aa70519058004e82
WS24 Agent <ws24-agent@local>
2026-03-03 12:21:25 +0000

    convert TextInputDialog to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/TextInputDialog.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/TextInputDialog.kt
@@ -16,149 +16,151 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2.dialogs;
+package com.googlecode.lanterna.gui2.dialogs
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.gui2.Button
+import com.googlecode.lanterna.gui2.DialogWindow
+import com.googlecode.lanterna.gui2.EmptySpace
+import com.googlecode.lanterna.gui2.GridLayout
+import com.googlecode.lanterna.gui2.Label
+import com.googlecode.lanterna.gui2.Panel
+import com.googlecode.lanterna.gui2.TextBox
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI
+import java.math.BigInteger
+import java.util.regex.Pattern
 
-import java.math.BigInteger;
-import java.util.regex.Pattern;
+open class TextInputDialog internal constructor(
+    title: String?,
+    description: String?,
+    textBoxPreferredSize: TerminalSize?,
+    initialContent: String?,
+    private val validator: TextInputDialogResultValidator?,
+    password: Boolean
+) : DialogWindow(title) {
 
-/**
- * {@code TextInputDialog} is a modal text input dialog that prompts the user to enter a text string. The class supports
- * validation and password masking. The builder class to help setup {@code TextInputDialog}s is
- * {@code TextInputDialogBuilder}.
- */
-public class TextInputDialog extends DialogWindow {
+    private val textBox: TextBox = TextBox(textBoxPreferredSize, initialContent)
+    private var result: String? = null
 
-    private final TextBox textBox;
-    private final TextInputDialogResultValidator validator;
-    private String result;
-
-    TextInputDialog(
-                String title,
-                String description,
-                TerminalSize textBoxPreferredSize,
-                String initialContent,
-                TextInputDialogResultValidator validator,
-                boolean password) {
-
-        super(title);
-        this.result = null;
-        this.textBox = new TextBox(textBoxPreferredSize, initialContent);
-        this.validator = validator;
-
-        if(password) {
-            textBox.setMask('*');
+    init {
+        if (password) {
+            textBox.setMask('*')
         }
 
-        Panel buttonPanel = new Panel();
-        buttonPanel.setLayoutManager(new GridLayout(2).setHorizontalSpacing(1));
-        buttonPanel.addComponent(new Button(LocalizedString.OK.toString(), this::onOK).setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.CENTER, true, false)));
-        buttonPanel.addComponent(new Button(LocalizedString.Cancel.toString(), this::onCancel));
+        val buttonPanel = Panel()
+        buttonPanel.setLayoutManager(GridLayout(2).setHorizontalSpacing(1))
+        buttonPanel.addComponent(
+            Button(LocalizedString.OK.toString(), this::onOK).setLayoutData(
+                GridLayout.createLayoutData(
+                    GridLayout.Alignment.CENTER,
+                    GridLayout.Alignment.CENTER,
+                    true,
+                    false
+                )
+            )
+        )
+        buttonPanel.addComponent(Button(LocalizedString.Cancel.toString(), this::onCancel))
 
-        Panel mainPanel = new Panel();
+        val mainPanel = Panel()
         mainPanel.setLayoutManager(
-                new GridLayout(1)
-                        .setLeftMarginSize(1)
-                        .setRightMarginSize(1));
-        if(description != null) {
-            mainPanel.addComponent(new Label(description));
+            GridLayout(1)
+                .setLeftMarginSize(1)
+                .setRightMarginSize(1)
+        )
+        if (description != null) {
+            mainPanel.addComponent(Label(description))
         }
-        mainPanel.addComponent(new EmptySpace(TerminalSize.ONE));
+        mainPanel.addComponent(EmptySpace(TerminalSize.ONE))
         textBox.setLayoutData(
-                GridLayout.createLayoutData(
-                        GridLayout.Alignment.FILL,
-                        GridLayout.Alignment.CENTER,
-                        true,
-                        false))
-                .addTo(mainPanel);
-        mainPanel.addComponent(new EmptySpace(TerminalSize.ONE));
+            GridLayout.createLayoutData(
+                GridLayout.Alignment.FILL,
+                GridLayout.Alignment.CENTER,
+                true,
+                false
+            )
+        ).addTo(mainPanel)
+        mainPanel.addComponent(EmptySpace(TerminalSize.ONE))
         buttonPanel.setLayoutData(
-                GridLayout.createLayoutData(
-                        GridLayout.Alignment.END,
-                        GridLayout.Alignment.CENTER,
-                        false,
-                        false))
-                .addTo(mainPanel);
-        setComponent(mainPanel);
+            GridLayout.createLayoutData(
+                GridLayout.Alignment.END,
+                GridLayout.Alignment.CENTER,
+                false,
+                false
+            )
+        ).addTo(mainPanel)
+        setComponent(mainPanel)
     }
 
-    private void onOK() {
-        String text = textBox.getText();
-        if(validator != null) {
-            String errorMessage = validator.validate(text);
-            if(errorMessage != null) {
-                MessageDialog.showMessageDialog(getTextGUI(), getTitle(), errorMessage, MessageDialogButton.OK);
-                return;
+    private fun onOK() {
+        val text = textBox.getText()
+        if (validator != null) {
+            val errorMessage = validator.validate(text)
+            if (errorMessage != null) {
+                MessageDialog.showMessageDialog(getTextGUI(), getTitle(), errorMessage, MessageDialogButton.OK)
+                return
             }
         }
-        result = text;
-        close();
+        result = text
+        close()
     }
 
-    private void onCancel() {
-        close();
+    private fun onCancel() {
+        close()
     }
 
-    @Override
-    public String showDialog(WindowBasedTextGUI textGUI) {
-        result = null;
-        super.showDialog(textGUI);
-        return result;
+    public override fun showDialog(textGUI: WindowBasedTextGUI?): String? {
+        result = null
+        super.showDialog(textGUI)
+        return result
     }
 
-    /**
-     * Shortcut for quickly showing a {@code TextInputDialog}
-     * @param textGUI GUI to show the dialog on
-     * @param title Title of the dialog
-     * @param description Description of the dialog
-     * @param initialContent What content to place in the text box initially
-     * @return The string the user typed into the text box, or {@code null} if the dialog was cancelled
-     */
-    public static String showDialog(WindowBasedTextGUI textGUI, String title, String description, String initialContent) {
-        TextInputDialog textInputDialog = new TextInputDialogBuilder()
+    companion object {
+        @JvmStatic
+        fun showDialog(
+            textGUI: WindowBasedTextGUI?,
+            title: String?,
+            description: String?,
+            initialContent: String?
+        ): String? {
+            val textInputDialog = TextInputDialogBuilder()
                 .setTitle(title)
                 .setDescription(description)
                 .setInitialContent(initialContent)
-                .build();
-        return textInputDialog.showDialog(textGUI);
-    }
+                .build()
+            return textInputDialog.showDialog(textGUI)
+        }
 
-    /**
-     * Shortcut for quickly showing a {@code TextInputDialog} that only accepts numbers
-     * @param textGUI GUI to show the dialog on
-     * @param title Title of the dialog
-     * @param description Description of the dialog
-     * @param initialContent What content to place in the text box initially
-     * @return The number the user typed into the text box, or {@code null} if the dialog was cancelled
-     */
-    public static BigInteger showNumberDialog(WindowBasedTextGUI textGUI, String title, String description, String initialContent) {
-        TextInputDialog textInputDialog = new TextInputDialogBuilder()
+        @JvmStatic
+        fun showNumberDialog(
+            textGUI: WindowBasedTextGUI?,
+            title: String?,
+            description: String?,
+            initialContent: String?
+        ): BigInteger? {
+            val textInputDialog = TextInputDialogBuilder()
                 .setTitle(title)
                 .setDescription(description)
                 .setInitialContent(initialContent)
                 .setValidationPattern(Pattern.compile("[0-9]+"), "Not a number")
-                .build();
-        String numberString = textInputDialog.showDialog(textGUI);
-        return numberString != null ? new BigInteger(numberString) : null;
-    }
+                .build()
+            val numberString = textInputDialog.showDialog(textGUI)
+            return if (numberString != null) BigInteger(numberString) else null
+        }
 
-    /**
-     * Shortcut for quickly showing a {@code TextInputDialog} with password masking
-     * @param textGUI GUI to show the dialog on
-     * @param title Title of the dialog
-     * @param description Description of the dialog
-     * @param initialContent What content to place in the text box initially
-     * @return The string the user typed into the text box, or {@code null} if the dialog was cancelled
-     */
-    public static String showPasswordDialog(WindowBasedTextGUI textGUI, String title, String description, String initialContent) {
-        TextInputDialog textInputDialog = new TextInputDialogBuilder()
+        @JvmStatic
+        fun showPasswordDialog(
+            textGUI: WindowBasedTextGUI?,
+            title: String?,
+            description: String?,
+            initialContent: String?
+        ): String? {
+            val textInputDialog = TextInputDialogBuilder()
                 .setTitle(title)
                 .setDescription(description)
                 .setInitialContent(initialContent)
                 .setPasswordInput(true)
-                .build();
-        return textInputDialog.showDialog(textGUI);
+                .build()
+            return textInputDialog.showDialog(textGUI)
+        }
     }
 }
