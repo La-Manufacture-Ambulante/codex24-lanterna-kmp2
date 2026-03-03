ae632d6ab5a1a2d04a523d39326e3f8540ff2d38
WS24 Agent <ws24-agent@local>
2026-03-03 14:17:24 +0000

    convert Wincon to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/win32/Wincon.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/win32/Wincon.kt
@@ -1,24 +1,45 @@
-package com.googlecode.lanterna.terminal.win32;
+package com.googlecode.lanterna.terminal.win32
 
-import com.sun.jna.Native;
-import com.sun.jna.platform.win32.WinDef.LPVOID;
-import com.sun.jna.platform.win32.WinNT.HANDLE;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.win32.StdCallLibrary;
-import com.sun.jna.win32.W32APIOptions;
+import com.sun.jna.Native
+import com.sun.jna.platform.win32.WinDef.LPVOID
+import com.sun.jna.platform.win32.WinNT.HANDLE
+import com.sun.jna.ptr.IntByReference
+import com.sun.jna.win32.StdCallLibrary
+import com.sun.jna.win32.W32APIOptions
 
-public interface Wincon extends StdCallLibrary, com.sun.jna.platform.win32.Wincon {
+interface Wincon : StdCallLibrary, com.sun.jna.platform.win32.Wincon {
+    fun GetConsoleScreenBufferInfo(
+        hConsoleOutput: HANDLE?,
+        lpConsoleScreenBufferInfo: WinDef.CONSOLE_SCREEN_BUFFER_INFO?
+    ): Boolean
 
-	Wincon INSTANCE = Native.load("kernel32", Wincon.class, W32APIOptions.UNICODE_OPTIONS);
+    fun ReadConsoleInput(
+        hConsoleInput: HANDLE?,
+        lpBuffer: Array<WinDef.INPUT_RECORD?>?,
+        nLength: Int,
+        lpNumberOfEventsRead: IntByReference?
+    ): Boolean
 
-	int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
-	int DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
-	int ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200;
+    fun WriteConsole(
+        hConsoleOutput: HANDLE?,
+        lpBuffer: String?,
+        nNumberOfCharsToWrite: Int,
+        lpNumberOfCharsWritten: IntByReference?,
+        lpReserved: LPVOID?
+    ): Boolean
 
-	boolean GetConsoleScreenBufferInfo(HANDLE hConsoleOutput, WinDef.CONSOLE_SCREEN_BUFFER_INFO lpConsoleScreenBufferInfo);
+    companion object {
+        @JvmField
+        val INSTANCE: Wincon =
+            Native.load("kernel32", Wincon::class.java, W32APIOptions.UNICODE_OPTIONS) as Wincon
 
-	boolean ReadConsoleInput(HANDLE hConsoleInput, WinDef.INPUT_RECORD[] lpBuffer, int nLength, IntByReference lpNumberOfEventsRead);
+        @JvmField
+        val ENABLE_VIRTUAL_TERMINAL_PROCESSING: Int = 0x0004
 
-	boolean WriteConsole(HANDLE hConsoleOutput, String lpBuffer, int nNumberOfCharsToWrite, IntByReference lpNumberOfCharsWritten, LPVOID lpReserved);
+        @JvmField
+        val DISABLE_NEWLINE_AUTO_RETURN: Int = 0x0008
 
+        @JvmField
+        val ENABLE_VIRTUAL_TERMINAL_INPUT: Int = 0x0200
+    }
 }
