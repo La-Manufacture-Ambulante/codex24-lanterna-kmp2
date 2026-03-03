8cfffdcbab5a2e94102cd6371d5635af9eb8939a
WS24 Agent <ws24-agent@local>
2026-03-03 14:17:23 +0000

    convert UnixTerminalSizeQuerier to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/ansi/UnixTerminalSizeQuerier.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/ansi/UnixTerminalSizeQuerier.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of lanterna (https://github.com/mabe02/lanterna).
- * 
+ *
  * lanterna is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,21 +13,20 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.terminal.ansi;
+package com.googlecode.lanterna.terminal.ansi
 
-import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TerminalSize
 
 /**
  * This class allows you to override by what means Lanterna detects the size of
- * the terminal. You can implement this interface and pass it to the 
+ * the terminal. You can implement this interface and pass it to the
  * UnixTerminal constructor in order to use it.
  * @author martin
  */
-@SuppressWarnings("WeakerAccess")
-public interface UnixTerminalSizeQuerier {
+interface UnixTerminalSizeQuerier {
     /**
      * Checks what the size of the terminal is, measured in number of rows and columns. The implementer of this
      * interface is expected to know which terminal we are querying for and have all it needs to figure out the size.
@@ -35,5 +34,5 @@ public interface UnixTerminalSizeQuerier {
      * a static size at all times.
      * @return Size of the terminal at this point in time
      */
-    TerminalSize queryTerminalSize();
+    fun queryTerminalSize(): TerminalSize?
 }
