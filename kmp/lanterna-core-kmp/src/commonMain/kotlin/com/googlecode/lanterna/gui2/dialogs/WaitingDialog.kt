4a7e2f43e39412cafcb30641517dd220de98520c
WS24 Agent <ws24-agent@local>
2026-03-03 12:21:25 +0000

    convert WaitingDialog to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/WaitingDialog.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/WaitingDialog.kt
@@ -1,81 +1,60 @@
-/*
- * This file is part of lanterna (https://github.com/mabe02/lanterna).
- *
- * lanterna is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2010-2020 Martin Berglund
- */
-package com.googlecode.lanterna.gui2.dialogs;
+package com.googlecode.lanterna.gui2.dialogs
 
-import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.*
 
-/**
- * Dialog that displays a text message, an optional spinning indicator and an optional progress bar. There is no buttons
- * in this dialog so it has to be explicitly closed through code.
- * @author martin
- */
-public class WaitingDialog extends DialogWindow {
-    private WaitingDialog(String title, String text) {
-        super(title);
-
-        Panel mainPanel = Panels.horizontal(
-                new Label(text),
-                AnimatedLabel.createClassicSpinningLine());
-        setComponent(mainPanel);
+open class WaitingDialog private constructor(title: String, text: String) : DialogWindow(title) {
+    init {
+        val mainPanel: Panel = Panels.horizontal(
+            Label(text),
+            AnimatedLabel.createClassicSpinningLine()
+        )
+        setComponent(mainPanel)
     }
 
-    @Override
-    public Object showDialog(WindowBasedTextGUI textGUI) {
-        showDialog(textGUI, true);
-        return null;
+    open override fun showDialog(textGUI: WindowBasedTextGUI): Any? {
+        showDialog(textGUI, true)
+        return null
     }
 
     /**
      * Displays the waiting dialog and optionally blocks until another thread closes it
      * @param textGUI GUI to add the dialog to
-     * @param blockUntilClosed If {@code true}, the method call will block until another thread calls {@code close()} on
+     * @param blockUntilClosed If `true`, the method call will block until another thread calls `close()` on
      *                         the dialog, otherwise the method call returns immediately
      */
-    public void showDialog(WindowBasedTextGUI textGUI, boolean blockUntilClosed) {
-        textGUI.addWindow(this);
+    open fun showDialog(textGUI: WindowBasedTextGUI, blockUntilClosed: Boolean) {
+        textGUI.addWindow(this)
 
-        if(blockUntilClosed) {
+        if (blockUntilClosed) {
             //Wait for the window to close, in case the window manager doesn't honor the MODAL hint
-            waitUntilClosed();
+            waitUntilClosed()
         }
     }
 
-    /**
-     * Creates a new waiting dialog
-     * @param title Title of the waiting dialog
-     * @param text Text to display on the waiting dialog
-     * @return Created waiting dialog
-     */
-    public static WaitingDialog createDialog(String title, String text) {
-        return new WaitingDialog(title, text);
-    }
+    companion object {
+        /**
+         * Creates a new waiting dialog
+         * @param title Title of the waiting dialog
+         * @param text Text to display on the waiting dialog
+         * @return Created waiting dialog
+         */
+        @JvmStatic
+        fun createDialog(title: String, text: String): WaitingDialog {
+            return WaitingDialog(title, text)
+        }
 
-    /**
-     * Creates and displays a waiting dialog without blocking for it to finish
-     * @param textGUI GUI to add the dialog to
-     * @param title Title of the waiting dialog
-     * @param text Text to display on the waiting dialog
-     * @return Created waiting dialog
-     */
-    public static WaitingDialog showDialog(WindowBasedTextGUI textGUI, String title, String text) {
-        WaitingDialog waitingDialog = createDialog(title, text);
-        waitingDialog.showDialog(textGUI, false);
-        return waitingDialog;
+        /**
+         * Creates and displays a waiting dialog without blocking for it to finish
+         * @param textGUI GUI to add the dialog to
+         * @param title Title of the waiting dialog
+         * @param text Text to display on the waiting dialog
+         * @return Created waiting dialog
+         */
+        @JvmStatic
+        fun showDialog(textGUI: WindowBasedTextGUI, title: String, text: String): WaitingDialog {
+            val waitingDialog = createDialog(title, text)
+            waitingDialog.showDialog(textGUI, false)
+            return waitingDialog
+        }
     }
 }
