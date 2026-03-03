86a71c1ca4264b2ae4339a5090e36cb0e906a50c
WS24 Agent <ws24-agent@local>
2026-03-03 14:17:23 +0000

    convert UnixTerminal to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/ansi/UnixTerminal.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/ansi/UnixTerminal.kt
@@ -16,31 +16,44 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.terminal.ansi;
+package com.googlecode.lanterna.terminal.ansi
 
-
-import java.io.*;
-import java.nio.charset.Charset;
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
 
 /**
  * This class extends UnixLikeTerminal and implements the Unix-specific parts.
- * <p>
+ *
  * If you need to have Lanterna to call stty at a different location, you'll need to
- * subclass this and override {@code getSTTYCommand()}.
+ * subclass this and override `getSTTYCommand()`.
  *
  * @author Martin
  */
-@SuppressWarnings("WeakerAccess")
-public class UnixTerminal extends UnixLikeTTYTerminal {
+@Suppress("WeakerAccess")
+class UnixTerminal private constructor(
+    terminalDevice: File,
+    terminalInput: InputStream,
+    terminalOutput: OutputStream,
+    terminalCharset: Charset,
+    terminalCtrlCBehaviour: CtrlCBehaviour
+) : UnixLikeTTYTerminal(
+    terminalDevice,
+    terminalInput,
+    terminalOutput,
+    terminalCharset,
+    terminalCtrlCBehaviour
+) {
 
     /**
      * Creates a UnixTerminal with default settings, using System.in and System.out for input/output, using the default
      * character set on the system as the encoding and trap ctrl+c signal instead of killing the application.
      * @throws IOException If there was an I/O error initializing the terminal
      */
-    public UnixTerminal() throws IOException {
-        this(System.in, System.out, Charset.defaultCharset());
-    }
+    @Throws(IOException::class)
+    constructor() : this(System.`in`, System.out, Charset.defaultCharset())
 
     /**
      * Creates a UnixTerminal using a specified input stream, output stream and character set. Ctrl+c signal will be
@@ -51,12 +64,12 @@ public class UnixTerminal extends UnixLikeTTYTerminal {
      * @param terminalCharset Character set to use when converting characters to bytes
      * @throws java.io.IOException If there was an I/O error initializing the terminal
      */
-    public UnixTerminal(
-            InputStream terminalInput,
-            OutputStream terminalOutput,
-            Charset terminalCharset) throws IOException {
-        this(terminalInput, terminalOutput, terminalCharset, CtrlCBehaviour.CTRL_C_KILLS_APPLICATION);
-    }
+    @Throws(IOException::class)
+    constructor(
+        terminalInput: InputStream,
+        terminalOutput: OutputStream,
+        terminalCharset: Charset
+    ) : this(terminalInput, terminalOutput, terminalCharset, CtrlCBehaviour.CTRL_C_KILLS_APPLICATION)
 
     /**
      * Creates a UnixTerminal using a specified input stream, output stream and character set, with a custom size
@@ -66,31 +79,16 @@ public class UnixTerminal extends UnixLikeTTYTerminal {
      * @param terminalInput Input stream to read terminal input from
      * @param terminalOutput Output stream to write terminal output to
      * @param terminalCharset Character set to use when converting characters to bytes
-     * @param terminalCtrlCBehaviour Special settings on how the terminal will behave, see {@code UnixTerminalMode} for more
+     * @param terminalCtrlCBehaviour Special settings on how the terminal will behave, see `UnixTerminalMode` for more
      * details
      * @throws java.io.IOException If there was an I/O error initializing the terminal
      */
-    @SuppressWarnings({"SameParameterValue", "WeakerAccess"})
-    public UnixTerminal(
-            InputStream terminalInput,
-            OutputStream terminalOutput,
-            Charset terminalCharset,
-            CtrlCBehaviour terminalCtrlCBehaviour) throws IOException {
-
-        this(new File("/dev/tty"), terminalInput, terminalOutput, terminalCharset, terminalCtrlCBehaviour);
-    }
-
-    private UnixTerminal(
-            File terminalDevice,
-            InputStream terminalInput,
-            OutputStream terminalOutput,
-            Charset terminalCharset,
-            CtrlCBehaviour terminalCtrlCBehaviour) throws IOException {
-
-        super(terminalDevice,
-                terminalInput,
-                terminalOutput,
-                terminalCharset,
-                terminalCtrlCBehaviour);
-    }
+    @Suppress("SameParameterValue", "WeakerAccess")
+    @Throws(IOException::class)
+    constructor(
+        terminalInput: InputStream,
+        terminalOutput: OutputStream,
+        terminalCharset: Charset,
+        terminalCtrlCBehaviour: CtrlCBehaviour
+    ) : this(File("/dev/tty"), terminalInput, terminalOutput, terminalCharset, terminalCtrlCBehaviour)
 }
