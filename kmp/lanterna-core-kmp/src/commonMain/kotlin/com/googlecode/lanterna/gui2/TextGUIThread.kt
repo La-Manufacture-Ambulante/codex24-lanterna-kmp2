7c4fd94ef1ad22f44014920a8d7c5c16ee138fe6
WS24 Agent <ws24-agent@local>
2026-03-03 11:52:18 +0000

    convert TextGUIThread to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/TextGUIThread.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/TextGUIThread.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of lanterna (https://github.com/mabe02/lanterna).
- * 
+ *
  * lanterna is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,15 +13,15 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2;
+package com.googlecode.lanterna.gui2
 
-import java.io.IOException;
+import java.io.IOException
 
 /**
- * Class that represents the thread this is expected to run the event/input/update loop for the {@code TextGUI}. There
+ * Class that represents the thread this is expected to run the event/input/update loop for the `TextGUI`. There
  * are mainly two implementations of this interface, one for having lanterna automatically spawn a new thread for doing
  * all the processing and leaving the creator thread free to do other things, and one that assumes the creator thread
  * will hand over control to lanterna for as long as the GUI is running.
@@ -29,37 +29,40 @@ import java.io.IOException;
  * @see SeparateTextGUIThread
  * @author Martin
  */
-public interface TextGUIThread {
+interface TextGUIThread {
     /**
-     * Invokes custom code on the GUI thread. Even if the current thread <b>is</b> the GUI thread, the code will be
+     * Invokes custom code on the GUI thread. Even if the current thread **is** the GUI thread, the code will be
      * executed at a later time when the event processing is done.
      *
      * @param runnable Code to run asynchronously
      * @throws java.lang.IllegalStateException If the GUI thread is not running
      */
-    void invokeLater(Runnable runnable) throws IllegalStateException;
+    @Throws(IllegalStateException::class)
+    fun invokeLater(runnable: Runnable?)
 
     /**
      * Main method to call when you are managing the event/input/update loop yourself. This method will run one round
      * through the GUI's event/input queue and update the visuals if required. If the operation did nothing (returning
-     * {@code false}) you could sleep for a millisecond and then try again. If you use {@code SameTextGUIThread} you
+     * `false`) you could sleep for a millisecond and then try again. If you use `SameTextGUIThread` you
      * must either call this method directly to make the GUI update or use one of the methods on
-     * {@code WindowBasedTextGUI} that blocks until a particular window has closed.
-     * @return {@code true} if there was anything to process or the GUI was updated, otherwise {@code false}
+     * `WindowBasedTextGUI` that blocks until a particular window has closed.
+     * @return `true` if there was anything to process or the GUI was updated, otherwise `false`
      * @throws IOException If there was an I/O error when processing and updating the GUI
      */
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
-    boolean processEventsAndUpdate() throws IOException;
+    @Suppress("BooleanMethodIsAlwaysInverted")
+    @Throws(IOException::class)
+    fun processEventsAndUpdate(): Boolean
 
     /**
      * Schedules custom code to be executed on the GUI thread and waits until the code has been executed before
-     * returning. If this is run on the GUI thread, it will immediately run the {@code Runnable} and then return.
+     * returning. If this is run on the GUI thread, it will immediately run the `Runnable` and then return.
      *
      * @param runnable Code to be run and waited for completion before this method returns
      * @throws IllegalStateException If the GUI thread is not running
      * @throws InterruptedException If the caller thread was interrupted while waiting for the task to be executed
      */
-    void invokeAndWait(Runnable runnable) throws IllegalStateException, InterruptedException;
+    @Throws(IllegalStateException::class, InterruptedException::class)
+    fun invokeAndWait(runnable: Runnable?)
 
     /**
      * Updates the exception handler used by this TextGUIThread. The exception handler will be invoked when an exception
@@ -67,13 +70,13 @@ public interface TextGUIThread {
      * thread or not.
      * @param exceptionHandler Handler to inspect exceptions
      */
-    void setExceptionHandler(ExceptionHandler exceptionHandler);
+    fun setExceptionHandler(exceptionHandler: ExceptionHandler?)
 
     /**
      * Returns the Java thread which is processing GUI events and updating the screen
      * @return Thread which is processing events and updating the screen
      */
-    Thread getThread();
+    fun getThread(): Thread?
 
     /**
      * This interface defines an exception handler, that is used for looking at exceptions that occurs during the main
@@ -84,15 +87,15 @@ public interface TextGUIThread {
         /**
          * Will be called when an IOException has occurred in the main event thread
          * @param e IOException that occurred
-         * @return If you return {@code true}, the event thread will be terminated
+         * @return If you return `true`, the event thread will be terminated
          */
-        boolean onIOException(IOException e);
+        fun onIOException(e: IOException?): Boolean
 
         /**
          * Will be called when a RuntimeException has occurred in the main event thread
          * @param e RuntimeException that occurred
-         * @return If you return {@code true}, the event thread will be terminated
+         * @return If you return `true`, the event thread will be terminated
          */
-        boolean onRuntimeException(RuntimeException e);
+        fun onRuntimeException(e: RuntimeException?): Boolean
     }
 }
