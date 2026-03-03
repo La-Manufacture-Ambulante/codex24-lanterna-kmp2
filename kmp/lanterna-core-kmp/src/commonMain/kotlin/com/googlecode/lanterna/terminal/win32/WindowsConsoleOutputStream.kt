07412220f75d27f26ab4210e3fbde4bd67472ee1
WS24 Agent <ws24-agent@local>
2026-03-03 14:17:24 +0000

    convert WindowsConsoleOutputStream to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/win32/WindowsConsoleOutputStream.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/terminal/win32/WindowsConsoleOutputStream.kt
@@ -1,60 +1,66 @@
-package com.googlecode.lanterna.terminal.win32;
+package com.googlecode.lanterna.terminal.win32
 
-import java.io.ByteArrayOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
+import com.sun.jna.platform.win32.WinNT.HANDLE
+import com.sun.jna.ptr.IntByReference
+import java.io.ByteArrayOutputStream
+import java.io.EOFException
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.charset.Charset
 
-import com.sun.jna.platform.win32.WinNT.HANDLE;
-import com.sun.jna.ptr.IntByReference;
+open class WindowsConsoleOutputStream : OutputStream {
+    private val hConsoleOutput: HANDLE?
+    private val decoderCharset: Charset?
+    private val buffer = ByteArrayOutputStream()
 
-public class WindowsConsoleOutputStream extends OutputStream {
+    constructor(decoder: Charset?) : this(
+        Wincon.INSTANCE.GetStdHandle(Wincon.STD_OUTPUT_HANDLE),
+        decoder
+    )
 
-	private final HANDLE hConsoleOutput;
-	private final Charset decoderCharset;
-	private final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    constructor(hConsoleOutput: HANDLE?, decoderCharset: Charset?) {
+        this.hConsoleOutput = hConsoleOutput
+        this.decoderCharset = decoderCharset
+    }
 
-	public WindowsConsoleOutputStream(Charset decoder) {
-		this(Wincon.INSTANCE.GetStdHandle(Wincon.STD_OUTPUT_HANDLE), decoder);
-	}
+    open fun getHandle(): HANDLE? {
+        return hConsoleOutput
+    }
 
-	public WindowsConsoleOutputStream(HANDLE hConsoleOutput, Charset decoderCharset) {
-		this.hConsoleOutput = hConsoleOutput;
-		this.decoderCharset = decoderCharset;
-	}
+    open fun getDecoderCharset(): Charset? {
+        return decoderCharset
+    }
 
-	public HANDLE getHandle() {
-		return hConsoleOutput;
-	}
+    @Synchronized
+    open override fun write(b: Int) {
+        buffer.write(b)
+    }
 
-	public Charset getDecoderCharset() {
-		return decoderCharset;
-	}
+    @Synchronized
+    open override fun write(b: ByteArray, off: Int, len: Int) {
+        buffer.write(b, off, len)
+    }
 
+    @Synchronized
+    @Throws(IOException::class)
+    open override fun flush() {
+        val charsetName = decoderCharset?.name() ?: throw NullPointerException()
+        var characters = buffer.toString(charsetName)
+        buffer.reset()
 
-	@Override
-	public synchronized void write(int b) {
-		buffer.write(b);
-	}
-
-	@Override
-	public synchronized void write(byte[] b, int off, int len) {
-		buffer.write(b, off, len);
-	}
-
-	@Override
-	public synchronized void flush() throws IOException {
-		String characters = buffer.toString(decoderCharset.name());
-		buffer.reset();
-
-		IntByReference lpNumberOfCharsWritten = new IntByReference();
-		while (!characters.isEmpty()) {
-			if (!Wincon.INSTANCE.WriteConsole(hConsoleOutput, characters, characters.length(), lpNumberOfCharsWritten, null)) {
-				throw new EOFException();
-			}
-			characters = characters.substring(lpNumberOfCharsWritten.getValue());
-		}
-	}
-
+        val lpNumberOfCharsWritten = IntByReference()
+        while (characters.isNotEmpty()) {
+            if (!Wincon.INSTANCE.WriteConsole(
+                    hConsoleOutput,
+                    characters,
+                    characters.length,
+                    lpNumberOfCharsWritten,
+                    null
+                )
+            ) {
+                throw EOFException()
+            }
+            characters = characters.substring(lpNumberOfCharsWritten.value)
+        }
+    }
 }
