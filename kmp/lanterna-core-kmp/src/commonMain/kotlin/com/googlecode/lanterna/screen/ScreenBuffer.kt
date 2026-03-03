815c562657a9b94b1db64ffd0592bcea1b75776d
WS24 Agent <ws24-agent@local>
2026-03-03 12:52:17 +0000

    convert ScreenBuffer to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/screen/ScreenBuffer.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/screen/ScreenBuffer.kt
@@ -16,137 +16,134 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.screen;
+package com.googlecode.lanterna.screen
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.BasicTextImage;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.graphics.TextImage;
+import com.googlecode.lanterna.TerminalPosition
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TextCharacter
+import com.googlecode.lanterna.graphics.BasicTextImage
+import com.googlecode.lanterna.graphics.TextGraphics
+import com.googlecode.lanterna.graphics.TextImage
 
-/**
- * Defines a buffer used by AbstractScreen and its subclasses to keep its state of what's currently displayed and what 
- * the edit buffer looks like. A ScreenBuffer is essentially a two-dimensional array of TextCharacter with some utility
- * methods to inspect and manipulate it in a safe way.
- * @author martin
- */
-public class ScreenBuffer implements TextImage {    
-    private final BasicTextImage backend;
-    
-    /**
-     * Creates a new ScreenBuffer with a given size and a TextCharacter to initially fill it with
-     * @param size Size of the buffer
-     * @param filler What character to set as the initial content of the buffer
-     */
-    public ScreenBuffer(TerminalSize size, TextCharacter filler) {
-        this(new BasicTextImage(size, filler));
+open class ScreenBuffer : TextImage {
+    private val backend: BasicTextImage
+
+    constructor(size: TerminalSize?, filler: TextCharacter?) : this(BasicTextImage(size, filler))
+
+    private constructor(backend: BasicTextImage) {
+        this.backend = backend
     }
-    
-    private ScreenBuffer(BasicTextImage backend) {
-        this.backend = backend;
+
+    override fun resize(newSize: TerminalSize?, filler: TextCharacter?): ScreenBuffer {
+        val resizedBackend = backend.resize(newSize, filler)
+        return ScreenBuffer(resizedBackend)
     }
-    
-    @Override
-    public ScreenBuffer resize(TerminalSize newSize, TextCharacter filler) {
-        BasicTextImage resizedBackend = backend.resize(newSize, filler);
-        return new ScreenBuffer(resizedBackend);
-    }
-    
-    boolean isVeryDifferent(ScreenBuffer other, int threshold) {
-        if(!getSize().equals(other.getSize())) {
-            throw new IllegalArgumentException("Can only call isVeryDifferent comparing two ScreenBuffers of the same size!"
-                    + " This is probably a bug in Lanterna.");
+
+    internal open fun isVeryDifferent(other: ScreenBuffer, threshold: Int): Boolean {
+        if (!size.equals(other.size)) {
+            throw IllegalArgumentException(
+                "Can only call isVeryDifferent comparing two ScreenBuffers of the same size!" +
+                    " This is probably a bug in Lanterna."
+            )
         }
-        int differences = 0;
-        for(int y = 0; y < getSize().getRows(); y++) {
-            for(int x = 0; x < getSize().getColumns(); x++) {
-                if(!getCharacterAt(x, y).equals(other.getCharacterAt(x, y))) {
-                    if(++differences >= threshold) {
-                        return true;
+        var differences = 0
+        for (y in 0 until size.rows) {
+            for (x in 0 until size.columns) {
+                if (!getCharacterAt(x, y).equals(other.getCharacterAt(x, y))) {
+                    if (++differences >= threshold) {
+                        return true
                     }
                 }
             }
         }
-        return false;
+        return false
     }
 
-    ///////////////////////////////////////////////////////////////////////////////
-    //  Delegate all TextImage calls (except resize) to the backend BasicTextImage
-    @Override
-    public TerminalSize getSize() {
-        return backend.getSize();
+    override fun getSize(): TerminalSize {
+        return backend.size
     }
 
-    @Override
-    public TextCharacter getCharacterAt(TerminalPosition position) {
-        return backend.getCharacterAt(position);
+    override fun getCharacterAt(position: TerminalPosition?): TextCharacter {
+        return backend.getCharacterAt(position)
     }
 
-    @Override
-    public TextCharacter getCharacterAt(int column, int row) {
-        return backend.getCharacterAt(column, row);
+    override fun getCharacterAt(column: Int, row: Int): TextCharacter {
+        return backend.getCharacterAt(column, row)
     }
 
-    @Override
-    public void setCharacterAt(TerminalPosition position, TextCharacter character) {
-        backend.setCharacterAt(position, character);
+    override fun setCharacterAt(position: TerminalPosition?, character: TextCharacter?) {
+        backend.setCharacterAt(position, character)
     }
 
-    @Override
-    public void setCharacterAt(int column, int row, TextCharacter character) {
-        backend.setCharacterAt(column, row, character);
+    override fun setCharacterAt(column: Int, row: Int, character: TextCharacter?) {
+        backend.setCharacterAt(column, row, character)
     }
 
-    @Override
-    public void setAll(TextCharacter character) {
-        backend.setAll(character);
+    override fun setAll(character: TextCharacter?) {
+        backend.setAll(character)
     }
 
-    @Override
-    public TextGraphics newTextGraphics() {
-        return backend.newTextGraphics();
+    override fun newTextGraphics(): TextGraphics {
+        return backend.newTextGraphics()
     }
 
-    @Override
-    public void copyTo(TextImage destination) {
-        if(destination instanceof ScreenBuffer) {
-            //This will allow the BasicTextImage's copy method to use System.arraycopy (micro-optimization?)
-            destination = ((ScreenBuffer)destination).backend;
+    override fun copyTo(destination: TextImage?) {
+        var actualDestination = destination
+        if (actualDestination is ScreenBuffer) {
+            actualDestination = actualDestination.backend
         }
-        backend.copyTo(destination);
+        backend.copyTo(actualDestination)
     }
 
-    @Override
-    public void copyTo(TextImage destination, int startRowIndex, int rows, int startColumnIndex, int columns, int destinationRowOffset, int destinationColumnOffset) {
-        if(destination instanceof ScreenBuffer) {
-            //This will allow the BasicTextImage's copy method to use System.arraycopy (micro-optimization?)
-            destination = ((ScreenBuffer)destination).backend;
+    override fun copyTo(
+        destination: TextImage?,
+        startRowIndex: Int,
+        rows: Int,
+        startColumnIndex: Int,
+        columns: Int,
+        destinationRowOffset: Int,
+        destinationColumnOffset: Int
+    ) {
+        var actualDestination = destination
+        if (actualDestination is ScreenBuffer) {
+            actualDestination = actualDestination.backend
         }
-        backend.copyTo(destination, startRowIndex, rows, startColumnIndex, columns, destinationRowOffset, destinationColumnOffset);
+        backend.copyTo(
+            actualDestination,
+            startRowIndex,
+            rows,
+            startColumnIndex,
+            columns,
+            destinationRowOffset,
+            destinationColumnOffset
+        )
     }
 
-    /**
-     * Copies the content from a TextImage into this buffer.
-     * @param source Source to copy content from
-     * @param startRowIndex Which row in the source image to start copying from
-     * @param rows How many rows to copy
-     * @param startColumnIndex Which column in the source image to start copying from
-     * @param columns How many columns to copy
-     * @param destinationRowOffset The row offset in this buffer of where to place the copied content
-     * @param destinationColumnOffset The column offset in this buffer of where to place the copied content
-     */
-    public void copyFrom(TextImage source, int startRowIndex, int rows, int startColumnIndex, int columns, int destinationRowOffset, int destinationColumnOffset) {
-        source.copyTo(backend, startRowIndex, rows, startColumnIndex, columns, destinationRowOffset, destinationColumnOffset);
+    open fun copyFrom(
+        source: TextImage,
+        startRowIndex: Int,
+        rows: Int,
+        startColumnIndex: Int,
+        columns: Int,
+        destinationRowOffset: Int,
+        destinationColumnOffset: Int
+    ) {
+        source.copyTo(
+            backend,
+            startRowIndex,
+            rows,
+            startColumnIndex,
+            columns,
+            destinationRowOffset,
+            destinationColumnOffset
+        )
     }
 
-    @Override
-    public void scrollLines(int firstLine, int lastLine, int distance) {
-        backend.scrollLines(firstLine, lastLine, distance);
+    override fun scrollLines(firstLine: Int, lastLine: Int, distance: Int) {
+        backend.scrollLines(firstLine, lastLine, distance)
     }
-    
-    @Override
-    public String toString() {
-        return backend.toString();
+
+    override fun toString(): String {
+        return backend.toString()
     }
 }
