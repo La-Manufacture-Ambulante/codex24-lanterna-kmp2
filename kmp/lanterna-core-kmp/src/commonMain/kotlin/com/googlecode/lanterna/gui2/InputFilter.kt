b34bf252c8762b1f63473ca4a4ba686248b76dd3
WS24 Agent <ws24-agent@local>
2026-03-03 11:52:17 +0000

    convert InputFilter to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/InputFilter.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/InputFilter.kt
@@ -16,21 +16,21 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2;
+package com.googlecode.lanterna.gui2
 
-import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyStroke
 
 /**
  * This interface can be used to programmatically intercept input from the user and decide if the input should be passed
  * on to the interactable. It's also possible to fire custom actions for certain keystrokes.
  */
-public interface InputFilter {
+interface InputFilter {
     /**
      * Called when the component is about to receive input from the user and decides if the input should be passed on to
      * the component or not
      * @param interactable Interactable that the input is directed to
      * @param keyStroke User input
-     * @return {@code true} if the input should be passed on to the interactable, {@code false} otherwise
+     * @return `true` if the input should be passed on to the interactable, `false` otherwise
      */
-    boolean onInput(Interactable interactable, KeyStroke keyStroke);
+    fun onInput(interactable: Interactable?, keyStroke: KeyStroke?): Boolean
 }
