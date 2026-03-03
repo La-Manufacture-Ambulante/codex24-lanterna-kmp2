5f9f02309b3fe8c82c91a62e57343003fcd6b11f
WS24 Agent <ws24-agent@local>
2026-03-03 12:21:25 +0000

    convert DialogWindow to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/DialogWindow.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/gui2/dialogs/DialogWindow.kt
@@ -16,42 +16,38 @@
  *
  * Copyright (C) 2010-2020 Martin Berglund
  */
-package com.googlecode.lanterna.gui2.dialogs;
+package com.googlecode.lanterna.gui2.dialogs
 
-import com.googlecode.lanterna.gui2.*;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import com.googlecode.lanterna.gui2.*
+import java.util.Collections
+import java.util.HashSet
+import java.util.Set
 
 /**
- * Thin layer on top of the {@code AbstractWindow} class that automatically sets properties and hints to the window to
+ * Thin layer on top of the `AbstractWindow` class that automatically sets properties and hints to the window to
  * make it act more like a modal dialog window
  */
-public abstract class DialogWindow extends AbstractWindow {
+public abstract class DialogWindow protected constructor(title: String?) : AbstractWindow(title) {
 
-    private static final Set<Hint> GLOBAL_DIALOG_HINTS =
-            Collections.unmodifiableSet(new HashSet<>(Collections.singletonList(Hint.MODAL)));
-
-    /**
-     * Default constructor, takes a title for the dialog and runs code shared for dialogs
-     * @param title Title of the window
-     */
-    protected DialogWindow(String title) {
-        super(title);
-        setHints(GLOBAL_DIALOG_HINTS);
+    init {
+        setHints(GLOBAL_DIALOG_HINTS)
     }
 
     /**
      * Opens the dialog by showing it on the GUI and doesn't return until the dialog has been closed
      * @param textGUI Text GUI to add the dialog to
-     * @return Depending on the {@code DialogWindow} implementation, by default {@code null}
+     * @return Depending on the `DialogWindow` implementation, by default `null`
      */
-    public Object showDialog(WindowBasedTextGUI textGUI) {
-        textGUI.addWindow(this);
+    public open fun showDialog(textGUI: WindowBasedTextGUI): Any? {
+        textGUI.addWindow(this)
 
         //Wait for the window to close, in case the window manager doesn't honor the MODAL hint
-        waitUntilClosed();
-        return null;
+        waitUntilClosed()
+        return null
+    }
+
+    private companion object {
+        private val GLOBAL_DIALOG_HINTS: Set<Hint> =
+            Collections.unmodifiableSet(HashSet(Collections.singletonList(Hint.MODAL)))
     }
 }
