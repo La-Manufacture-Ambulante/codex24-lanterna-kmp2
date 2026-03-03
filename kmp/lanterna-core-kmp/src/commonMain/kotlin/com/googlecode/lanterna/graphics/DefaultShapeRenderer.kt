9af90f4dc7c828406cc14e319818cda52fd33a17
WS24 Agent <ws24-agent@local>
2026-03-03 10:30:58 +0000

    convert DefaultShapeRenderer to Kotlin (optimistic pass)

--- kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/graphics/DefaultShapeRenderer.kt
+++ kmp/lanterna-core-kmp/src/commonMain/kotlin/com/googlecode/lanterna/graphics/DefaultShapeRenderer.kt
@@ -1,202 +1,221 @@
-/*
- * This file is part of lanterna (https://github.com/mabe02/lanterna).
- *
- * lanterna is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2010-2020 Martin Berglund
- */
-package com.googlecode.lanterna.graphics;
+package com.googlecode.lanterna.graphics
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TerminalPosition
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.TextCharacter
+import java.util.Arrays
+import java.util.Comparator
 
-import java.util.Arrays;
-import java.util.Comparator;
-
-/**
- * Default implementation of ShapeRenderer. This class (and the interface) is mostly here to make the code cleaner in
- * {@code AbstractTextGraphics}.
- * @author Martin
- */
-class DefaultShapeRenderer implements ShapeRenderer {
-    interface Callback {
-        void onPoint(int column, int row, TextCharacter character);
+internal open class DefaultShapeRenderer(private val callback: Callback) : ShapeRenderer {
+    internal interface Callback {
+        fun onPoint(column: Int, row: Int, character: TextCharacter)
     }
 
-    private final Callback callback;
-
-    DefaultShapeRenderer(Callback callback) {
-        this.callback = callback;
-    }
-
-    @Override
-    public void drawLine(TerminalPosition p1, TerminalPosition p2, TextCharacter character) {
+    override fun drawLine(p1: TerminalPosition, p2: TerminalPosition, character: TextCharacter) {
         //http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
         //Implementation from Graphics Programming Black Book by Michael Abrash
         //Available at http://www.gamedev.net/page/resources/_/technical/graphics-programming-and-theory/graphics-programming-black-book-r1698
-        if(p1.getRow() > p2.getRow()) {
-            TerminalPosition temp = p1;
-            p1 = p2;
-            p2 = temp;
+        var p1Var = p1
+        var p2Var = p2
+        if (p1Var.row > p2Var.row) {
+            val temp = p1Var
+            p1Var = p2Var
+            p2Var = temp
         }
-        int deltaX = p2.getColumn() - p1.getColumn();
-        int deltaY = p2.getRow() - p1.getRow();
-        if(deltaX > 0) {
-            if(deltaX > deltaY) {
-                drawLine0(p1, deltaX, deltaY, true, character);
-            }
-            else {
-                drawLine1(p1, deltaX, deltaY, true, character);
-            }
-        }
-        else {
-            deltaX = Math.abs(deltaX);
-            if(deltaX > deltaY) {
-                drawLine0(p1, deltaX, deltaY, false, character);
-            }
-            else {
-                drawLine1(p1, deltaX, deltaY, false, character);
-            }
-        }
-    }
-
-    private void drawLine0(TerminalPosition start, int deltaX, int deltaY, boolean leftToRight, TextCharacter character) {
-        int x = start.getColumn();
-        int y = start.getRow();
-        int deltaYx2 = deltaY * 2;
-        int deltaYx2MinusDeltaXx2 = deltaYx2 - (deltaX * 2);
-        int errorTerm = deltaYx2 - deltaX;
-        callback.onPoint(x, y, character);
-        while(deltaX-- > 0) {
-            if(errorTerm >= 0) {
-                y++;
-                errorTerm += deltaYx2MinusDeltaXx2;
-            }
-            else {
-                errorTerm += deltaYx2;
-            }
-            x += leftToRight ? 1 : -1;
-            callback.onPoint(x, y, character);
-        }
-    }
-
-    private void drawLine1(TerminalPosition start, int deltaX, int deltaY, boolean leftToRight, TextCharacter character) {
-        int x = start.getColumn();
-        int y = start.getRow();
-        int deltaXx2 = deltaX * 2;
-        int deltaXx2MinusDeltaYx2 = deltaXx2 - (deltaY * 2);
-        int errorTerm = deltaXx2 - deltaY;
-        callback.onPoint(x, y, character);
-        while(deltaY-- > 0) {
-            if(errorTerm >= 0) {
-                x += leftToRight ? 1 : -1;
-                errorTerm += deltaXx2MinusDeltaYx2;
-            }
-            else {
-                errorTerm += deltaXx2;
-            }
-            y++;
-            callback.onPoint(x, y, character);
-        }
-    }
-
-    @Override
-    public void drawTriangle(TerminalPosition p1, TerminalPosition p2, TerminalPosition p3, TextCharacter character) {
-        drawLine(p1, p2, character);
-        drawLine(p2, p3, character);
-        drawLine(p3, p1, character);
-    }
-
-    @Override
-    public void drawRectangle(TerminalPosition topLeft, TerminalSize size, TextCharacter character) {
-        TerminalPosition topRight = topLeft.withRelativeColumn(size.getColumns() - 1);
-        TerminalPosition bottomRight = topRight.withRelativeRow(size.getRows() - 1);
-        TerminalPosition bottomLeft = topLeft.withRelativeRow(size.getRows() - 1);
-        drawLine(topLeft, topRight, character);
-        drawLine(topRight, bottomRight, character);
-        drawLine(bottomRight, bottomLeft, character);
-        drawLine(bottomLeft, topLeft, character);
-    }
-
-    @Override
-    public void fillTriangle(TerminalPosition p1, TerminalPosition p2, TerminalPosition p3, TextCharacter character) {
-        //I've used the algorithm described here:
-        //http://www-users.mat.uni.torun.pl/~wrona/3d_tutor/tri_fillers.html
-        TerminalPosition[] points = new TerminalPosition[]{p1, p2, p3};
-        Arrays.sort(points, Comparator.comparingInt(TerminalPosition::getRow));
-
-        float dx1, dx2, dx3;
-        if (points[1].getRow() - points[0].getRow() > 0) {
-            dx1 = (float)(points[1].getColumn() - points[0].getColumn()) / (float)(points[1].getRow() - points[0].getRow());
-        }
-        else {
-            dx1 = 0;
-        }
-        if (points[2].getRow() - points[0].getRow() > 0) {
-            dx2 = (float)(points[2].getColumn() - points[0].getColumn()) / (float)(points[2].getRow() - points[0].getRow());
-        }
-        else {
-            dx2 = 0;
-        }
-        if (points[2].getRow() - points[1].getRow() > 0) {
-            dx3 = (float)(points[2].getColumn() - points[1].getColumn()) / (float)(points[2].getRow() - points[1].getRow());
-        }
-        else {
-            dx3 = 0;
-        }
-
-        float startX, startY, endX;
-        startX = endX = points[0].getColumn();
-        startY =        points[0].getRow();
-        if (dx1 > dx2) {
-            for (; startY <= points[1].getRow(); startY++, startX += dx2, endX += dx1) {
-                drawLine(new TerminalPosition((int)startX, (int)startY), new TerminalPosition((int)endX, (int)startY), character);
-            }
-            endX = points[1].getColumn();
-            for (; startY <= points[2].getRow(); startY++, startX += dx2, endX += dx3) {
-                drawLine(new TerminalPosition((int)startX, (int)startY), new TerminalPosition((int)endX, (int)startY), character);
+        var deltaX = p2Var.column - p1Var.column
+        val deltaY = p2Var.row - p1Var.row
+        if (deltaX > 0) {
+            if (deltaX > deltaY) {
+                drawLine0(p1Var, deltaX, deltaY, true, character)
+            } else {
+                drawLine1(p1Var, deltaX, deltaY, true, character)
             }
         } else {
-            for (; startY <= points[1].getRow(); startY++, startX += dx1, endX += dx2) {
-                drawLine(new TerminalPosition((int)startX, (int)startY), new TerminalPosition((int)endX, (int)startY), character);
-            }
-            startX = points[1].getColumn();
-            startY = points[1].getRow();
-            for (; startY <= points[2].getRow(); startY++, startX += dx3, endX += dx2) {
-                drawLine(new TerminalPosition((int)startX, (int)startY), new TerminalPosition((int)endX, (int)startY), character);
+            deltaX = kotlin.math.abs(deltaX)
+            if (deltaX > deltaY) {
+                drawLine0(p1Var, deltaX, deltaY, false, character)
+            } else {
+                drawLine1(p1Var, deltaX, deltaY, false, character)
             }
         }
     }
 
-    @Override
-    public void fillRectangle(TerminalPosition topLeft, TerminalSize size, TextCharacter character) {
-        final boolean characterDoubleWidth = character.isDoubleWidth();
-        for(int y = 0; y < size.getRows(); y++) {
-            for(int x = 0; x < size.getColumns(); x++) {
+    private fun drawLine0(
+        start: TerminalPosition,
+        deltaX: Int,
+        deltaY: Int,
+        leftToRight: Boolean,
+        character: TextCharacter
+    ) {
+        var x = start.column
+        var y = start.row
+        val deltaYx2 = deltaY * 2
+        val deltaYx2MinusDeltaXx2 = deltaYx2 - (deltaX * 2)
+        var errorTerm = deltaYx2 - deltaX
+        callback.onPoint(x, y, character)
+        var remainingDeltaX = deltaX
+        while (remainingDeltaX-- > 0) {
+            if (errorTerm >= 0) {
+                y++
+                errorTerm += deltaYx2MinusDeltaXx2
+            } else {
+                errorTerm += deltaYx2
+            }
+            x += if (leftToRight) 1 else -1
+            callback.onPoint(x, y, character)
+        }
+    }
+
+    private fun drawLine1(
+        start: TerminalPosition,
+        deltaX: Int,
+        deltaY: Int,
+        leftToRight: Boolean,
+        character: TextCharacter
+    ) {
+        var x = start.column
+        var y = start.row
+        val deltaXx2 = deltaX * 2
+        val deltaXx2MinusDeltaYx2 = deltaXx2 - (deltaY * 2)
+        var errorTerm = deltaXx2 - deltaY
+        callback.onPoint(x, y, character)
+        var remainingDeltaY = deltaY
+        while (remainingDeltaY-- > 0) {
+            if (errorTerm >= 0) {
+                x += if (leftToRight) 1 else -1
+                errorTerm += deltaXx2MinusDeltaYx2
+            } else {
+                errorTerm += deltaXx2
+            }
+            y++
+            callback.onPoint(x, y, character)
+        }
+    }
+
+    override fun drawTriangle(
+        p1: TerminalPosition,
+        p2: TerminalPosition,
+        p3: TerminalPosition,
+        character: TextCharacter
+    ) {
+        drawLine(p1, p2, character)
+        drawLine(p2, p3, character)
+        drawLine(p3, p1, character)
+    }
+
+    override fun drawRectangle(topLeft: TerminalPosition, size: TerminalSize, character: TextCharacter) {
+        val topRight = topLeft.withRelativeColumn(size.columns - 1)
+        val bottomRight = topRight.withRelativeRow(size.rows - 1)
+        val bottomLeft = topLeft.withRelativeRow(size.rows - 1)
+        drawLine(topLeft, topRight, character)
+        drawLine(topRight, bottomRight, character)
+        drawLine(bottomRight, bottomLeft, character)
+        drawLine(bottomLeft, topLeft, character)
+    }
+
+    override fun fillTriangle(
+        p1: TerminalPosition,
+        p2: TerminalPosition,
+        p3: TerminalPosition,
+        character: TextCharacter
+    ) {
+        //I've used the algorithm described here:
+        //http://www-users.mat.uni.torun.pl/~wrona/3d_tutor/tri_fillers.html
+        val points = arrayOf(p1, p2, p3)
+        Arrays.sort(points, Comparator.comparingInt(TerminalPosition::getRow))
+
+        val dx1: Float
+        val dx2: Float
+        val dx3: Float
+        if (points[1].row - points[0].row > 0) {
+            dx1 = (points[1].column - points[0].column).toFloat() / (points[1].row - points[0].row).toFloat()
+        } else {
+            dx1 = 0f
+        }
+        if (points[2].row - points[0].row > 0) {
+            dx2 = (points[2].column - points[0].column).toFloat() / (points[2].row - points[0].row).toFloat()
+        } else {
+            dx2 = 0f
+        }
+        if (points[2].row - points[1].row > 0) {
+            dx3 = (points[2].column - points[1].column).toFloat() / (points[2].row - points[1].row).toFloat()
+        } else {
+            dx3 = 0f
+        }
+
+        var startX: Float
+        var startY: Float
+        var endX: Float
+        startX = points[0].column.toFloat()
+        endX = points[0].column.toFloat()
+        startY = points[0].row.toFloat()
+        if (dx1 > dx2) {
+            while (startY <= points[1].row.toFloat()) {
+                drawLine(
+                    TerminalPosition(startX.toInt(), startY.toInt()),
+                    TerminalPosition(endX.toInt(), startY.toInt()),
+                    character
+                )
+                startY++
+                startX += dx2
+                endX += dx1
+            }
+            endX = points[1].column.toFloat()
+            while (startY <= points[2].row.toFloat()) {
+                drawLine(
+                    TerminalPosition(startX.toInt(), startY.toInt()),
+                    TerminalPosition(endX.toInt(), startY.toInt()),
+                    character
+                )
+                startY++
+                startX += dx2
+                endX += dx3
+            }
+        } else {
+            while (startY <= points[1].row.toFloat()) {
+                drawLine(
+                    TerminalPosition(startX.toInt(), startY.toInt()),
+                    TerminalPosition(endX.toInt(), startY.toInt()),
+                    character
+                )
+                startY++
+                startX += dx1
+                endX += dx2
+            }
+            startX = points[1].column.toFloat()
+            startY = points[1].row.toFloat()
+            while (startY <= points[2].row.toFloat()) {
+                drawLine(
+                    TerminalPosition(startX.toInt(), startY.toInt()),
+                    TerminalPosition(endX.toInt(), startY.toInt()),
+                    character
+                )
+                startY++
+                startX += dx3
+                endX += dx2
+            }
+        }
+    }
+
+    override fun fillRectangle(topLeft: TerminalPosition, size: TerminalSize, character: TextCharacter) {
+        val characterDoubleWidth = character.isDoubleWidth
+        var y = 0
+        while (y < size.rows) {
+            var x = 0
+            while (x < size.columns) {
                 // Don't put a double-width character at the right edge of the area
-                if (characterDoubleWidth && x + 1 == size.getColumns()) {
-                    callback.onPoint(topLeft.getColumn() + x, topLeft.getRow() + y, character.withCharacter(' '));
-                }
-                else {
+                if (characterDoubleWidth && x + 1 == size.columns) {
+                    callback.onPoint(topLeft.column + x, topLeft.row + y, character.withCharacter(' '))
+                } else {
                     // Default case
-                    callback.onPoint(topLeft.getColumn() + x, topLeft.getRow() + y, character);
+                    callback.onPoint(topLeft.column + x, topLeft.row + y, character)
                 }
                 if (characterDoubleWidth) {
-                    x++;
+                    x++
                 }
+                x++
             }
+            y++
         }
     }
 }
