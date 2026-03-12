cee28304e01388c44c459cb25baee0e001f4a1ea
WS24 Agent <ws24-agent@local>
2026-03-12 09:55:15 +0000

    ci: align pr4 with pr3 ktlint task wiring

--- kmp/lanterna-demo-kmp/build.gradle.kts
+++ kmp/lanterna-demo-kmp/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform") version "2.1.21"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
 kotlin {
@@ -19,13 +20,13 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
+        val commonMain by getting
+        val commonTest by getting
+        val jvmMain by getting {
             dependencies {
                 implementation(project(":lanterna-core-kmp"))
             }
         }
-        val commonTest by getting
-        val jvmMain by getting
         val jvmTest by getting
     }
 }
